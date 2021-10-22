290f8e90aac21acd908ff02a20bc0417d725a11c
Sludgadin <strazeconsulting@gmail.com>
2021-10-22 18:46:02 +1100

    try simply changing config

--- data/firebase/config.tsx
+++ data/firebase/config.tsx
@@ -3,7 +3,7 @@ import { initializeApp, getApp } from 'firebase/app';
 //2. Initialize app with the config vars
 const app = initializeApp({
     apiKey: "AIzaSyAU62kOE6xhSrFqoXQPv6_WHxYilmoUxDk",
-    authDomain: "idlemmo.firebaseapp.com",
+    authDomain: "localhost",
     databaseURL: "idlemmo.firebaseio.com",
     projectId: "idlemmo",
 });
