99fd0615aeb9ebe9a7d56ec3ce0939dd441517ab
Sludgadin <strazeconsulting@gmail.com>
2021-10-22 22:59:37 +1100

    experimental id token based auth

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,9 +5,11 @@ import {
     Header,
     Text,
     Button,
-    Box
+    Box,
+    Layer,
+    TextInput
 } from "grommet"
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AppContext } from '../data/appContext'
 import { AuthContext } from '../data/firebase/authContext'
 
@@ -26,18 +28,41 @@ export default function Layout({
     const user = authData?.user;
     const idleonData = useContext(AppContext);
 
-    const onLoginClick = (loginFunction: Function | undefined) => {
-        if (loginFunction) {
-            loginFunction();
+    const [showLayer, setShowLayer] = useState(false);
+    const [value, setValue] = useState('');
+
+    const onButtonClick = (toCall: Function | undefined, value?: string) => {
+        if (toCall) {
+            if (value) {
+                toCall(value);
+            }
+            else {
+                toCall();
+            }
         }
     }
+
     return (
         <Container>
             <Header background="light-4" pad="medium" height="xsmall">
                 <Box justify="between" direction="row" gap="medium" width="100%">
                     <Text>Last Updated: {idleonData.getLastUpdated()}</Text>
-                    {user && <React.Fragment><Text>Logged in as: {user?.displayName}</Text><Button onClick={() => onLoginClick(authData?.logoutFunction)}>Logout</Button></React.Fragment>}
-                    {!user && <Button onClick={() => onLoginClick(authData?.loginFunction)}>Login</Button>}
+                    {user && <React.Fragment><Text>Logged in as: {user?.displayName}</Text><Button onClick={() => onButtonClick(authData?.logoutFunction)}>Logout</Button></React.Fragment>}
+                    {!user && <Button onClick={() => setShowLayer(true)}>Login</Button>}
+                    {showLayer &&
+                        <Layer
+                            onEsc={() => setShowLayer(false)}
+                            onClickOutside={() => setShowLayer(false)}
+                        >
+                            <Button label="Google Login" onClick={() => onButtonClick(authData?.loginFunction)} />
+                            <TextInput
+                                placeholder="type here"
+                                value={value}
+                                onChange={event => setValue(event.target.value)}
+                            />
+                            <Button label="Handle Token" onClick={() => onButtonClick(authData?.tokenFunction, value)} />
+                        </Layer>
+                    }
                 </Box>
             </Header>
             <main>{children}</main>
