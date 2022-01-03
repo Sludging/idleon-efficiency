35e6accd522a1f8a282fb9c5d9e33576cb2fa375
Sludgadin <strazeconsulting@gmail.com>
2022-01-03 23:58:28 +1100

    obol inventory

--- pages/account/obols.tsx
+++ pages/account/obols.tsx
@@ -2,6 +2,7 @@ import {
     Box,
     Grid,
     Heading,
+    Text,
 } from "grommet"
 
 import { useEffect, useState, useContext } from 'react';
@@ -11,7 +12,71 @@ import { NextSeo } from 'next-seo';
 import { Player } from "../../data/domain/player";
 import LeftNavButton from "../../components/base/LeftNavButton";
 import ObolsInfo from "../../components/account/task-board/obolsInfo";
+import { ObolsData, ObolType } from "../../data/domain/obols";
+import TextAndLabel from "../../components/base/TextAndLabel";
+import TipDisplay, { TipDirection } from "../../components/base/TipDisplay";
+import { ItemStat } from "../../data/domain/items";
 
+function ObolInventory() {
+    const [obolsData, setObolsData] = useState<ObolsData>();
+    const idleonData = useContext(AppContext);
+
+    const statsDisplay = (stats: ItemStat[], description: string) => {
+        if (description) {
+            return <Text>{description}</Text>
+        }
+
+        return stats.filter((stat) => stat.shouldDisplay()).map((x, index) => <Text key={index}>{x.getDisplay()}</Text>);
+    }
+
+    useEffect(() => {
+        const theData = idleonData.getData();
+        setObolsData(theData.get("obols"));
+    }, [idleonData])
+
+    return (
+        <Box pad="medium" gap="small">
+            <Text size="large">Inventory</Text>
+            {
+                obolsData && [...obolsData.inventory].map(([type, obols], index) => (
+                    <Box key={index} pad="small" gap="small" border={index + 1 < obolsData.inventory.size ? {side: 'bottom', color: 'grey-1', size: '2px'} : undefined}>
+                        <Text size="medium">{ObolType[type]}</Text>
+                        <Box direction="row">
+                            {
+                                obols.filter(obol => !obol.locked && obol.item.internalName != "Blank").sort((obol1, obol2) => obol1.getRarity() > obol2.getRarity() ? -1 : 1).map((obol, obolIndex) => {
+                                    return (
+                                        <TipDisplay
+                                            key={obolIndex}
+                                            heading={`${obol.item.displayName} (${obol.item.type})`}
+                                            body={<Box>{statsDisplay(obol.item.itemStats, obol.item.description)}<Text size="xsmall">*A work in progress, therefore not always accurate.</Text></Box>}
+                                            size={"large"}
+                                            direction={TipDirection.Down}
+                                            maxWidth="large"
+                                        >
+                                            <Box width={{ max: '36px', min: '36px' }}>
+                                                <Box className={obol.item.getClass()} />
+                                            </Box>
+                                        </TipDisplay>
+                                    )
+                                })
+                            }
+                        </Box>
+                        <Box direction="row" gap="medium">
+                        <TextAndLabel
+                            label={"Empty Slots"}
+                            text={obols.filter(obol => obol.item.internalName == "Blank").length.toString() ?? "0"}
+                        />
+                        <TextAndLabel
+                            label={"Total Slots"}
+                            text={obols.filter(obol => !obol.item.internalName.includes("Locked")).length.toString() ?? "0"}
+                        />
+                        </Box>
+                    </Box>
+                ))
+            }
+        </Box>
+    )
+}
 
 function Obols() {
     const [playerData, setPlayerData] = useState<Player[]>();
@@ -34,6 +99,7 @@ function Obols() {
                 <Grid rows="1" columns={['25%', '75%']}>
                     <Box pad="medium" height="100%">
                         <LeftNavButton key={-1} isActive={index == -1} clickHandler={() => onActive(-1)} text={"Family"} />
+                        <LeftNavButton key={-2} isActive={index == -2} clickHandler={() => onActive(-2)} text={"Inventory"} />
                         {
                             playerData?.map((player, playerIndex) => {
                                 return (
@@ -43,7 +109,11 @@ function Obols() {
                         }
                     </Box>
                     <Box fill background="dark-1">
-                        <ObolsInfo playerIndex={index} title={index == -1 ? "Family" : playerData && playerData.length > index && playerData[index].playerName ? playerData[index].playerName : "Unknown"} level={index == -1 ? playerData?.reduce((sum, player) => sum += player.level, 0) ?? 0 : playerData && playerData.length > index && playerData[index].level ? playerData[index].level : 0} />
+                        {index == -2 ?
+                            <ObolInventory />
+                            :
+                            <ObolsInfo playerIndex={index} title={index == -1 ? "Family" : playerData && playerData.length > index && playerData[index].playerName ? playerData[index].playerName : "Unknown"} level={index == -1 ? playerData?.reduce((sum, player) => sum += player.level, 0) ?? 0 : playerData && playerData.length > index && playerData[index].level ? playerData[index].level : 0} />
+                        }
                     </Box>
                 </Grid>
             </ShadowBox>
