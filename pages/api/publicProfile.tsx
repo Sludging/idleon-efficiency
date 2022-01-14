825206b57f5baab3e391305f6edef270ad2cf400
Sludgadin <strazeconsulting@gmail.com>
2022-01-14 16:01:30 +1100

    fix broken local code

--- pages/api/publicProfile.tsx
+++ pages/api/publicProfile.tsx
@@ -1,13 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import data from '../../profiles/sludger.json';
-import data2 from '../../profiles/morojo.json';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.query.profile == "sludger") {
     res.status(200).json(data);
-  }
-  else if (req.query.profile == "morojo") {
-    res.status(200).json(data2);
   }
   else {
     res.status(404).json({ message: `Profile ${req.query.profile} not found.`});
