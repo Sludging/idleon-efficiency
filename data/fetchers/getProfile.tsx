c04ec59e17d616279f89346ea1e8eaebca95a224
Sludgadin <strazeconsulting@gmail.com>
2022-01-14 16:40:05 +1100

    add print and ignore www

--- data/fetchers/getProfile.tsx
+++ data/fetchers/getProfile.tsx
@@ -2,11 +2,12 @@ import { IdleonStorage } from "../firebase/storage";
 
 export const fetcher = async (windowLocation: string, oldDomain: string): Promise<{ data: Map<string, any> | undefined, charNames: string[] | undefined, domain: string }> => {
     let urlDomain = "";
+    console.log("Working under", process.env.NEXT_PUBLIC_VERCEL_URL, process.env.NEXT_PUBLIC_ROOT_URL);
     if (windowLocation != "") {
         const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_ROOT_URL || "localhost:3000";
         urlDomain = window.location.host.replace(baseURL, "").replace('.', '');
     }
-    if (urlDomain != oldDomain && urlDomain != "") {
+    if (urlDomain != oldDomain && urlDomain != "" && urlDomain != "www") {
         if (process.env.NEXT_PUBLIC_APP_STAGE == "dev") {
             console.log("Calling local storage")
             try {
