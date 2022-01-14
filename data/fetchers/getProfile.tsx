a34fd77d44076e6db426bb11012f62eb798a13c3
Sludgadin <strazeconsulting@gmail.com>
2022-01-14 16:45:27 +1100

    prioritise root url variable

--- data/fetchers/getProfile.tsx
+++ data/fetchers/getProfile.tsx
@@ -4,7 +4,7 @@ export const fetcher = async (windowLocation: string, oldDomain: string): Promis
     let urlDomain = "";
     console.log("Working under", process.env.NEXT_PUBLIC_VERCEL_URL, process.env.NEXT_PUBLIC_ROOT_URL);
     if (windowLocation != "") {
-        const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_ROOT_URL || "localhost:3000";
+        const baseURL = process.env.NEXT_PUBLIC_ROOT_URL || process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
         urlDomain = window.location.host.replace(baseURL, "").replace('.', '');
     }
     if (urlDomain != oldDomain && urlDomain != "" && urlDomain != "www") {
