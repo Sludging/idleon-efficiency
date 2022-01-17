eb88d27b322e6d2077f9c7b9d232a42b11cbd24e
Sludgadin <strazeconsulting@gmail.com>
2022-01-17 18:56:54 +1100

    add upload profile button

--- data/storage/profiles.tsx
+++ data/storage/profiles.tsx
@@ -1,31 +1,46 @@
-const cdn_location: string = 'https://d3vsh1slqhmor3.cloudfront.net/profiles'
+import { User } from "firebase/auth";
+import { IdleonData } from "../domain/idleonData";
+
+const cdn_location: string = 'https://cdn.idleonefficiency.com'
+const api_location: string = "https://api.idleonefficiency.com/publish-profile"
 
 export class ProfileStorage {
     static _Initialize() {
-        this._Initialize = () => {} // prevent any additional calls to initialize.
+        this._Initialize = () => { } // prevent any additional calls to initialize.
     }
 
-    // static uploadProfile = async (data: IdleonData, user: User, profile: string) => {
-    //     const storageRef = ref(this.storage, `/profile/${profile}.json`);
-    //     try {
-    //         const jsonData = JSON.stringify(data.getData());
-    //         await uploadString(storageRef, jsonData, StringFormat.RAW, {
-    //             contentType: 'application/json',
-    //             customMetadata: {
-    //                 owner: user.uid
-    //             }
-    //         });
-    //     }
-    //     catch (e) {
-    //         console.debug(e);
-    //         return false;
-    //     }
-    //     return true;
-    // }
+    static uploadProfile = async (data: IdleonData, user: User) => {
+        try {
+            const theData = data.getData();
+            const uploadData = JSON.stringify({
+                rawData: theData.get("rawData"),
+                playerNames: theData.get("playerNames")
+            })
+            const userJWT = await user.getIdToken();
+            const res = await fetch(api_location,
+                {
+                    method: 'POST',
+                    body: uploadData,
+                    headers: [
+                        ["Authorization", `Bearer ${userJWT}`],
+                        ["Content-Type", "application/json"],
+                    ]
+                });
+            if (res.ok) {
+                const jsonData = await res.json();
+                return jsonData;
+            }
+        }
+        catch (e) {
+            console.debug(e);
+            return false;
+        }
+        return true;
+    }
 
     static downloadProfile = async (profile: string) => {
         try {
-            const res = await fetch(`${cdn_location}/${profile}.json`);
+            const res = await fetch(`${cdn_location}/profiles/${profile}.json`);
             if (res.ok) {
                 const jsonData = await res.json();
                 return jsonData;
