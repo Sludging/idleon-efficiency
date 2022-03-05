3e85381059c5aa0660e3fdb270809718b73681e2
Sludgadin <strazeconsulting@gmail.com>
2022-03-05 17:56:32 +1100

    add metadata and skipping unknown categories

--- pages/leaderboards.tsx
+++ pages/leaderboards.tsx
@@ -1,21 +1,26 @@
-import { Anchor, Box, Footer, Grid, Header, Heading, Main, ResponsiveContext, Text } from "grommet"
+import { Anchor, Box, Footer, Grid, Header, Heading, Main, ResponsiveContext } from "grommet"
 import Image from "next/image";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
+import ShadowBox from "../components/base/ShadowBox";
+import TextAndLabel from "../components/base/TextAndLabel";
 import Category from "../components/leaderboards/category";
 import { AppContext, AppStatus } from "../data/appContext";
-import { LeaderboardsData } from "../data/domain/leaderboards/data";
+import { LeaderboardsData, TitleMap } from "../data/domain/leaderboards/data";
 import { Player } from "../data/domain/player";
+import { dateToText } from "../data/utility";
 
 const PointerImage = styled(Image)`
     cursor: pointer;
 `
 
-const Leaderboards = ({ leaderboards } : { leaderboards: LeaderboardsData }) => {
+const Leaderboards = ({ leaderboards }: { leaderboards: LeaderboardsData }) => {
     const [currentUser, setCurrentUser] = useState<string | undefined>(undefined);
     const size = useContext(ResponsiveContext);
     const appContext = useContext(AppContext);
+
+    const categoryUpdateTime = new Date(leaderboards.data[0].UpdatedAt);
 
     useEffect(() => {
         if (appContext.status == AppStatus.LiveData) {
@@ -26,7 +31,7 @@ const Leaderboards = ({ leaderboards } : { leaderboards: LeaderboardsData }) => 
             }
         }
     }, [appContext]);
-    
+
     return (
         <Box
             flex
@@ -40,24 +45,54 @@ const Leaderboards = ({ leaderboards } : { leaderboards: LeaderboardsData }) => 
                             <PointerImage alt="Logo" src="/logo.svg" height="21px" width="171px" />
                         </Box>
                     </Link>
+                    <Box pad="medium">
+                        <TextAndLabel textColor='accent-3' textSize='xsmall' labelSize='xsmall' label='Last Updated' text={dateToText(categoryUpdateTime)} />
                     </Box>
+                </Box>
             </Header>
             <Main>
                 <Box width={{ max: '1440px' }} margin={{ left: 'auto', right: 'auto' }} fill="horizontal">
                     <Box pad={{ right: 'large', left: 'large', bottom: 'medium' }}>
-                        <Box fill align="center" margin={{bottom: 'small'}}>
-                            <Heading margin={{bottom: 'small'}} level="1">Leaderboards</Heading>
-                            {/* <Box justify="start">
-                                <Text color="grey-2">Total profiles: 0</Text>
-                                <Text color="grey-2">Profiles updated in the last 7 days: 0</Text>
-                            </Box> */}
+                        <Box fill align="center" margin={{ bottom: 'small' }}>
+                            <Heading margin={{ bottom: 'small' }} level="1">Leaderboards</Heading>
                         </Box>
+                        <Box pad={{ horizontal: "medium" }} fill direction="row" justify="between" gap="small">
+                            <ShadowBox background="dark-1" pad="medium" fill>
+                                <TextAndLabel
+                                    labelSize="xsmall"
+                                    textSize="xxlarge"
+                                    label="Total Profiles"
+                                    text={leaderboards.metadata.TotalProfiles.toString()}
+                                />
+                            </ShadowBox>
+                            <ShadowBox background="dark-1" pad="medium" fill>
+                                <TextAndLabel
+                                    labelSize="xsmall"
+                                    textSize="xxlarge"
+                                    label="Updated in the last week"
+                                    text={leaderboards.metadata.UpdatedRecently.toString()}
+                                />
+                            </ShadowBox>
+                        </Box>
+                        {/* <Box direction="row" pad="medium" gap="medium">
+                            {
+                                Object.keys(CategoryGroup).map((group, index) => {
+                                    if (isNaN(parseInt(group))) {
+                                        return (
+                                            <Box key={index}>
+                                                <Button style={{ color: "white" }} primary color="brand" label={group} />
+                                            </Box>
+                                        )
+                                    }
+                                })
+                            }
+                        </Box> */}
                         <Grid columns={size == "small" ? "1" : "1/3"} pad="medium" gap="small">
-                        {
-                            leaderboards.data.map((category, index) => (
-                                <Category key={index} data={category} currentUser={currentUser} />
-                            ))
-                        }
+                            {
+                                leaderboards.data.sort((category1, category2) => (TitleMap.get(category1.Category)?.group ?? "ZZZZ") > (TitleMap.get(category2.Category)?.group ?? "ZZZZ") ? -1 : 1).map((category, index) => (
+                                    <Category key={index} data={category} currentUser={currentUser} />
+                                ))
+                            }
                         </Grid>
                     </Box>
                 </Box>
@@ -83,13 +118,13 @@ export async function getStaticProps() {
     // You can use any data fetching library
     const res = await fetch('https://api.idleonefficiency.com/leaderboards')
     const leaderboards = await res.json()
-  
+
     return {
-      props: {
-        leaderboards,
-      },
-      revalidate: 1800,
+        props: {
+            leaderboards,
+        },
+        revalidate: 1800,
     }
-  }
+}
 
 export default Leaderboards;
