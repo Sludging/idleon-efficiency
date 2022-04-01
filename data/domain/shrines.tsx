b1c909cc91220c3bbf0028f5acefb3ac42fb99d5
Sludgadin <LLJKSom0@gmail.com>
2022-04-01 14:43:02 +1100

    1.52 updates

--- data/domain/shrines.tsx
+++ data/domain/shrines.tsx
@@ -14,17 +14,17 @@ export class Shrine {
     level: number = 0;
     accumulatedHours: number = 0;
 
-    worldTour: boolean = false;    
-    constructor(public name: string, public boost: string, public initialBonus: number, public increment: number, public iconName: string) {}
+    worldTour: boolean = false;
+    constructor(public name: string, public boost: string, public initialBonus: number, public increment: number, public iconName: string) { }
 
     isShrineActive = (map: number | undefined = undefined) => {
         // if map is valid, and we are on the same map, shrine is active.
-        if (map != undefined &&  map == this.currentMap) {
+        if (map != undefined && map == this.currentMap) {
             return true;
         }
 
         // if map is valid and world tour is active, check if shrine in the same world.
-        
+
         if (map != undefined && this.worldTour) {
             const playerWorld = Math.floor(map / 50);
             const shrineWorld = Math.floor(this.currentMap / 50);
@@ -49,7 +49,7 @@ export class Shrine {
 
     getHourRequirement = () => {
         return Math.floor(20 * (this.level - 1) + 6 * this.level * Math.pow(1.63, this.level - 1))
-    } 
+    }
 
     getClass = () => {
         return `icons-6666 icons-${this.iconName}`;
@@ -58,15 +58,15 @@ export class Shrine {
 
 const initShrines = () => {
     return [
-    new Shrine("Woodular Shrine", "Boosts Total Damage of players on this map by +%. @ AFK Time to next Lv: @", 12, 3, "ConTowerB18"),
-    new Shrine("Isaccian Shrine", "Boosts Max HP and DEF of players on this map by +%. @ AFK Time to next Lv: @", 12, 3, "ConTowerB19"),
-    new Shrine("Crystal Shrine", "Boosts the Lv Up Rate of all Shrines on this map by +%. @ AFK Time to next Lv: @", 20, 4, "ConTowerB20"),
-    new Shrine("Pantheon Shrine", "Boosts Carry Capacity of players on this map by +%. @ AFK Time to next Lv: @", 10, 2, "ConTowerB21"),
-    new Shrine("Clover Shrine", "Boosts Drop Rate of players on this map by +%. @ AFK Time to next Lv: @", 15, 3, "ConTowerB22"),
-    new Shrine("Summereading Shrine", "Boosts ALL Exp Gain of players on this map by +%. @ AFK Time to next Lv: @", 6, 1, "ConTowerB23"),
-    new Shrine("Crescent Shrine", "Boosts Crystal and Giant Spawn chance on this map by +%. @ AFK Time to next Lv: @", 50, 10, "ConTowerB24"),
-    new Shrine("Undead Shrine", "Boosts Respawn Rate of monsters on this map by +%. @ AFK Time to next Lv: @", 5, 1, "ConTowerB25"),
-    new Shrine("Primordial Shrine", "Boosts AFK Gain Rate on this map by +%, up to 80% total. @ AFK Time to next Lv: @", 5, 1, "ConTowerB26"),
+        new Shrine("Woodular Shrine", "Boosts Total Damage of players on this map by +%. @ AFK Time to next Lv: @", 12, 3, "ConTowerB18"),
+        new Shrine("Isaccian Shrine", "Boosts Max HP and DEF of players on this map by +%. @ AFK Time to next Lv: @", 12, 3, "ConTowerB19"),
+        new Shrine("Crystal Shrine", "Boosts the Lv Up Rate of all Shrines on this map by +%. @ AFK Time to next Lv: @", 20, 4, "ConTowerB20"),
+        new Shrine("Pantheon Shrine", "Boosts Carry Capacity of players on this map by +%. @ AFK Time to next Lv: @", 10, 2, "ConTowerB21"),
+        new Shrine("Clover Shrine", "Boosts Drop Rate of players on this map by +%. @ AFK Time to next Lv: @", 15, 3, "ConTowerB22"),
+        new Shrine("Summereading Shrine", "Boosts ALL Exp Gain of players on this map by +%. @ AFK Time to next Lv: @", 6, 1, "ConTowerB23"),
+        new Shrine("Crescent Shrine", "Boosts Crystal and Giant Spawn chance on this map by +%. @ AFK Time to next Lv: @", 50, 10, "ConTowerB24"),
+        new Shrine("Undead Shrine", "Boosts Respawn Rate of monsters on this map by +%. @ AFK Time to next Lv: @", 5, 1, "ConTowerB25"),
+        new Shrine("Primordial Shrine", "Boosts AFK Gain Rate on this map by +%, up to 80% total. @ AFK Time to next Lv: @", 1, 0.1, "ConTowerB26"),
     ];
 }
 
