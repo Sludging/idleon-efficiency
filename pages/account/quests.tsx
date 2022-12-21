2111c7e1ae8caa18b8262e5c8812f9c66c92b107
Sludgadin <LLJKSom0@gmail.com>
2022-12-21 17:50:35 +1100

    update quests and npcs

--- pages/account/quests.tsx
+++ pages/account/quests.tsx
@@ -12,13 +12,15 @@ import ShadowBox from "../../components/base/ShadowBox";
 import { AppContext } from '../../data/appContext';
 import { NextSeo } from 'next-seo';
 import { Player } from "../../data/domain/player";
-import { NPC, Quests as QuestInfo, NpcQuest } from "../../data/domain/quests";
+import { NPC, QuestInformation as SpecialQuestInfo, Quests as QuestInfo } from "../../data/domain/quests";
 import { Next } from 'grommet-icons';
 import { MouseEventHandler } from "hoist-non-react-statics/node_modules/@types/react";
 import TipDisplay, { TipDirection } from "../../components/base/TipDisplay";
 import CoinsDisplay from "../../components/coinsDisplay";
 import { getCoinsArray } from "../../data/utility";
 import IconImage from "../../components/base/IconImage";
+import { QuestTypeEnum } from "../../data/domain/enum/questTypeEnum";
+import { ImageData } from "../../data/domain/imageData";
 
 enum CharacterBoxStatus {
     Complete,
@@ -48,7 +50,7 @@ function CharacterBox({ player, status }: { player: Player, status: CharacterBox
     return (
         <Box title={titleForStatus(status)} background="dark-2" align="center" pad={{ top: "xsmall", bottom: "xsmall", left: "small", right: "small" }} gap="xsmall" direction="row" border={{ size: '1px', color: borderColorForStatus(status) }}>
             <Box style={{ opacity: status == CharacterBoxStatus.Disabled ? 0.4 : 1 }}>
-                <IconImage data={player.getClassImageData()} scale={0.7}/>
+                <IconImage data={player.getClassImageData()} scale={0.7} />
             </Box>
             <Box>
                 <Text color="grey-2" size="12px" truncate={true}>{player.playerName}</Text>
@@ -57,16 +59,14 @@ function CharacterBox({ player, status }: { player: Player, status: CharacterBox
     )
 }
 
-function SpecialButton({ isActive, text, icon, clickHandler }: { isActive: boolean, text: string, icon?: string, clickHandler: MouseEventHandler }) {
+function SpecialButton({ isActive, text, imageData, clickHandler }: { isActive: boolean, text: string, imageData?: ImageData, clickHandler: MouseEventHandler }) {
     return (
         <Button fill="horizontal" plain active={isActive} onClick={clickHandler} gap="medium">
             <Box background={isActive ? 'accent-4' : 'dark-2'} pad={{ left: 'medium', right: 'small', top: 'xsmall', bottom: 'xsmall' }} direction="row" justify="between" align="center" gap="small" height="40px">
                 <Box direction="row" gap="small" align="center">
                     {
-                        icon && 
-                        <Box width={{max: '30px', min: '30px'}}>
-                            <Box className={icon} />
-                        </Box>
+                        imageData &&
+                        <IconImage data={imageData} scale={0.75}/>
                     }
                     <Text color='accent-2' size="small" weight={isActive ? 'bold' : 'normal'}>{text}</Text>
                 </Box>
@@ -86,45 +86,47 @@ function TabButton({ isActive, text, clickHandler }: { isActive: boolean, text: 
     )
 }
 
-function QuestInformation({ info }: { info: NpcQuest}) {
+function QuestInformation({ info }: { info: SpecialQuestInfo }) {
 
     return (
         <Box gap="small">
-            <Text weight="bold">Requirements:{ info.ConsumeItems ? " (Consumed on delivery)" : ""}</Text>
-            { info.Type == "ItemsAndSpaceRequired" && 
-                info.ItemReq?.map((item, index) => (
-                        <Text key={`req_${index}`} size="small">Collect {item.displayName}: {item.quantity}</Text>
+            <Text weight="bold">Requirements:{info.ConsumeItems ? " (Consumed on delivery)" : ""}</Text>
+            {info.QuestType == QuestTypeEnum.ItemsAndSpaceRequired &&
+                info.ItemReqs.map((item, index) => (
+                    <Text key={`req_${index}`} size="small">Collect {item.displayName}: {item.count}</Text>
                 ))
             }
-            { 
-              info.Type == "Custom" && 
-                info.CustomArray?.map((data, index) => (
+            {
+                info.QuestType == QuestTypeEnum.Custom &&
+                info.CustomArray.map((data, index) => (
                     <Text key={`req_${index}`} size="small">{data.desc} {data.finalV}</Text>
                 ))
             }
             <Text weight="bold">Rewards:</Text>
             {
-                info.Rewards?.map((item, index) => {
-                    if (item.item == "TalentBook1" || item.item?.includes("Recipe")) {
+                info.Rewards.map((item, index) => {
+                    if (item.internalName == "TalentBook1" || item.internalName?.includes("Recipe")) {
                         return (<Text key={`reward_${index}`} size="small">Talent Book or Recipe</Text>)
                     }
-                    if (item.item) {
+                    if (item.internalName) {
                         return (
                             <Box key={`reward_${index}`} direction="row" gap="small" align="center">
-                                <IconImage data={{ location: item.item, height: 36, width: 36 }} />
-                                <Text size="small">{item.displayName ?? item.item}: {item.quantity}</Text>
+                                <IconImage data={item.getImageData()} />
+                                <Text size="small">{item.displayName ?? item.internalName}: {item.count}</Text>
                             </Box>
-                             )
-                    }
-                    if (item.type == "Class" || item.type == "Smithing" || item.type == "Choppin" || item.type == "Mining") {
-                        return (
-                            <Text key={`reward_${index}`}>{item.amount} {item.type} Exp</Text>
                         )
                     }
-                    if (item.coins) {
-                        return (<CoinsDisplay key={`reward_${index}`} coinMap={getCoinsArray(item.coins)} />)
+                    // TODO: FIX THIS!
+                    if (item.type == "Class" || item.type == "Smithing" || item.type == "Choppin" || item.type == "Mining" || item.type == "Experience0") {
+                        return (
+                            <Text key={`reward_${index}`}>{item.count} {item.type} Exp</Text>
+                        )
                     }
-                    return (<Text key={`reward_${index}`}>{item.type} | {item.coins} | {item.amount}</Text>)
+                    // TODO: FIX THIS!
+                    if (item.internalName == "") {
+                        return (<CoinsDisplay key={`reward_${index}`} coinMap={getCoinsArray(item.count)} />)
+                    }
+                    return (<Text key={`reward_${index}`}>{item.type} | {item.count}</Text>)
                 })
             }
             <Text size="xsmall">*A work in progress, therefore not always accurate.</Text>
@@ -137,9 +139,7 @@ function NPCQuests({ npc, playerInfo, playerQuestData }: { npc: NPC, playerInfo:
     return (
         <Box gap="small">
             <Box direction="row" align="center" gap="small">
-                <Box width={{ min: '50px', max: '50px' }}>
-                    <Box className={npc.getClass()} />
-                </Box>
+                <IconImage data={npc.getImageData()} />
                 <Text size="medium">{npc.name}</Text>
             </Box>
             <Box pad={{ left: 'large' }}>
@@ -147,15 +147,15 @@ function NPCQuests({ npc, playerInfo, playerQuestData }: { npc: NPC, playerInfo:
                     <Box key={index} pad={{ top: 'small', bottom: 'small' }} border={{ side: 'bottom', color: 'white-1' }}>
                         <TipDisplay
                             heading={`${info.Name} (Difficulty: ${info.Difficulty})`}
-                            body={<QuestInformation info={info} />}
+                            body={<QuestInformation info={npc.getQuestInformation(info.Name!)} />}
                             size={"large"}
                             direction={TipDirection.Down}
                             maxWidth="large">
-                            <Box pad={{ top: "xsmall", bottom: "xsmall"}}>
+                            <Box pad={{ top: "xsmall", bottom: "xsmall" }}>
                                 <Text size="small">{info.Name}</Text>
                             </Box>
                         </TipDisplay>
-                        <Box pad={{ top: "small", bottom: "small"}} gap="small">
+                        <Box pad={{ top: "small", bottom: "small" }} gap="small">
                             <Grid columns={{ size: '120px' }} gap="small">
                                 {
                                     playerInfo.map((player, index) => {
@@ -207,10 +207,10 @@ function Quests() {
 
         // world filter
         if (activeWorld != "Events") {
-            filtered = filtered.filter(([name, info]) => info.getWorld() == activeWorld && info.data.head.type != "Event");
+            filtered = filtered.filter(([name, info]) => info.data.head?.world == activeWorld && info.data.head.type != "Event");
         }
         else {
-            filtered = filtered.filter(([name, info]) => info.data.head.type == "Event");
+            filtered = filtered.filter(([name, info]) => info.data.head?.type == "Event");
         }
 
         return filtered.sort(([name1], [name2]) => name1 > name2 ? 1 : -1);
@@ -233,7 +233,11 @@ function Quests() {
                     <TabButton isActive={activeWorld == "Yum-Yum Desert"} text="World 2" clickHandler={() => { setActiveWorld("Yum-Yum Desert"); onActive(1) }} />
                     <TabButton isActive={activeWorld == "Frostbite Tundra"} text="World 3" clickHandler={() => { setActiveWorld("Frostbite Tundra"); onActive(1) }} />
                     <TabButton isActive={activeWorld == "Hyperion Nebula"} text="World 4" clickHandler={() => { setActiveWorld("Hyperion Nebula"); onActive(1) }} />
+                    <TabButton isActive={activeWorld == "Smolderin' Plateau"} text="World 5" clickHandler={() => { setActiveWorld("Smolderin' Plateau"); onActive(1) }} />
                     <TabButton isActive={activeWorld == "Events"} text="Events" clickHandler={() => { setActiveWorld("Events"); onActive(1) }} />
+                </Box>
+                <Box pad={{ horizontal: "large", top: "small" }}>
+                    <Text size="xsmall">* Exp and Coin rewards currently don't display properly, it's a known issue.</Text>
                 </Box>
                 <ShadowBox flex={false}>
                     <Grid rows="1" columns={size == "small" ? ['40%', '60%'] : ['25%', '75%']}>
@@ -241,7 +245,7 @@ function Quests() {
                             {
                                 npcsToShow.map(([name, info], npcIndex) => {
                                     return (
-                                        <SpecialButton icon={info.getClass()} key={npcIndex} isActive={index == npcIndex + 1} clickHandler={() => onActive(npcIndex + 1)} text={name} />
+                                        <SpecialButton imageData={info.getImageData()} key={npcIndex} isActive={index == npcIndex + 1} clickHandler={() => onActive(npcIndex + 1)} text={name} />
                                     )
                                 })
                             }
