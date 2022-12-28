4746ab1cbf37949b501f15da498c2e30dd795d16
Sludgadin <LLJKSom0@gmail.com>
2022-12-28 22:20:50 +1100

    some sailing

--- components/layout.tsx
+++ components/layout.tsx
@@ -180,6 +180,8 @@ export default function Layout({
         {
             link: "/world-5", label: "World 5", subLinks: [
                 { subLink: "/divinity", label: "Divinity" },
+                { subLink: "/sailing", label: "Sailing" },
+                { subLink: "/gaming", label: "Gaming" },
             ]
         },
         { link: "/players", label: "Players" },
