58c9ff3805e33e9f0fc72780b6d4b9df14c18957
Sludgadin <LLJKSom0@gmail.com>
2023-01-12 23:04:22 +1100

    first pass fix cooking math

--- data/domain/cooking.tsx
+++ data/domain/cooking.tsx
@@ -1,6 +1,7 @@
 import { nFormatter, round } from "../utility"
 import { Achievement } from "./achievements";
 import { Alchemy } from "./alchemy";
+import { AtomCollider } from "./atomCollider";
 import { Breeding } from "./breeding";
 import { Card } from "./cards";
 import { initMealRepo, MealBase } from "./data/MealRepo";
@@ -9,6 +10,7 @@ import { ImageData } from "./imageData";
 import { Lab } from "./lab";
 import { MealModel } from "./model/mealModel";
 import { Player } from "./player";
+import { Sailing } from "./sailing";
 import { Sigils } from "./sigils";
 import { Stamp } from "./stamps";
 
@@ -174,12 +176,13 @@ export class Kitchen {
 
     constructor(public index: number) { }
 
-    getMealSpeed = (vialBonus: number, stampBonus: number, mealCookBonus: number, jewelBonus: number, cardBonus: number, kitchenEffBonus: number, jewelBonus2: number, diamonChef: number, achieve225: boolean, achieve224: boolean) => {
-        const baseMath = 10 * (1 + (this.richelin ? 2 : 0)) * Math.max(1, diamonChef);
+    getMealSpeed = (vialBonus: number, stampBonus: number, mealCookBonus: number, jewelBonus: number, cardBonus: number, kitchenEffBonus: number, jewelBonus2: number, diamonChef: number, achieve225: boolean, achieve224: boolean, atom8Bonus: number, artifact13Bonus: number) => {
+        const baseMath = 10 * (1 + (this.richelin ? 2 : 0)) * Math.max(1, diamonChef) * Math.max(1, atom8Bonus);
+        const moreMath = (1 + this.mealLevels / 10) * (1 + artifact13Bonus / 100); 
         const bonusMath = (1 + (stampBonus + Math.max(0, jewelBonus2)) / 100) * (1 + mealCookBonus / 100) * Math.max(1, jewelBonus);
         const cardAndAchiImpact = 1 + Math.min(cardBonus + (20 * (achieve225 ? 1 : 0)) + (10 * (achieve224 ? 1 : 0)), 100) / 100;
         return baseMath *
-            (1 + this.mealLevels / 10) *
+            moreMath *
             (1 + vialBonus / 100) *
             bonusMath *
             cardAndAchiImpact *
@@ -389,6 +392,8 @@ export const updateCooking = (data: Map<string, any>) => {
     const achievements = data.get("achievements") as Achievement[];
     const sigils = data.get("sigils") as Sigils;
     const players = data.get("players") as Player[];
+    const sailing = data.get("sailing") as Sailing;
+    const collider = data.get("collider") as AtomCollider;
 
     const bestLadleSkillLevel = Math.max(...players.flatMap(player => (player.talents.find(talent => talent.skillIndex == 148)?.maxLevel ?? 0)));
     if (bestLadleSkillLevel > 0) {
@@ -416,6 +421,8 @@ export const updateCooking = (data: Map<string, any>) => {
     const jewelBonus = mainframe.jewels[0].getBonus(); // TODO: Remove hardcoding
     const jewelBonus2 = mainframe.jewels[14].getBonus(); // TODO: Remove hardcoding
     const cardBonus = cards.find(card => card.id == "Boss4A")?.getBonus() ?? 0;
+    const artifactBonus = sailing.artifacts[13].getBonus();
+    const atomBonus = collider.atoms[8].getBonus();
 
     // Fire speed
     const fireVialBonus = alchemy.vials.filter(vial => vial.description.includes("New Recipe Cooking Speed")).reduce((sum, vial) => sum += vial.getBonus(), 0);
@@ -429,7 +436,7 @@ export const updateCooking = (data: Map<string, any>) => {
 
     let totalContribution = 0;
     cooking.kitchens.forEach(kitchen => {
-        kitchen.mealSpeed = kitchen.getMealSpeed(vialBonus, stampBonus, mealSpeedBonus, jewelBonus, cardBonus, kitchenEfficientBonus, jewelBonus2, diamonChef, achievements[225].completed, achievements[224].completed);
+        kitchen.mealSpeed = kitchen.getMealSpeed(vialBonus, stampBonus, mealSpeedBonus, jewelBonus, cardBonus, kitchenEfficientBonus, jewelBonus2, diamonChef, achievements[225].completed, achievements[224].completed, atomBonus, artifactBonus);
         kitchen.fireSpeed = kitchen.getFireSpeed(fireVialBonus, fireStampBonus, fireSpeedMealBonus, cardBonus, kitchenEfficientBonus, diamonChef);
         kitchen.recipeLuck = kitchen.getLuck();
 
