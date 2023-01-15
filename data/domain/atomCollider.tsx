8c32849dd261f9c59fe42d50ea4353c9353c4a8c
Sludgadin <LLJKSom0@gmail.com>
2023-01-15 20:48:29 +1100

    atom cost math

--- data/domain/atomCollider.tsx
+++ data/domain/atomCollider.tsx
@@ -2,13 +2,26 @@ import { Cooking } from './cooking';
 import { Construction } from './construction';
 import { AtomColliderBase, initAtomColliderRepo } from './data/AtomColliderRepo';
 import { AtomColliderModel } from './model/atomColliderModel';
+import { Alchemy } from './alchemy';
 
 export class Atom {
     level: number = 0;
+
+    // Cost math related
+    nenoBonus: number = 0;
+    colliderBuildingLevel: number = 0;
+    bubbleBonus: number = 0; // Y5
+
     constructor(public index: number, public data: AtomColliderModel) { }
 
     getBonus = (): number => {
         return this.level * this.data.bonusPerLv;
+    }
+
+    getCost = (level: number = this.level): number => {
+        return (1 / 1 + (this.nenoBonus + (this.bubbleBonus + this.colliderBuildingLevel / 10)) / 100) * 
+        (this.data.baseCost + level * this.data.growthFactor) *
+        Math.pow(this.data.baseExponent, this.index);
     }
 
     static fromBase = (data: AtomColliderBase[]) => {
@@ -68,14 +81,25 @@ export default function parseAtomCollider(atomsData: number[], divinityData: num
 }
 
 export function updateAtomCollider(data: Map<string, any>) {
-    const collider = data.get("collider");
+    const collider = data.get("collider") as AtomCollider;
     const optLacc = data.get("OptLacc");
     const cooking = data.get("cooking") as Cooking;
     const construction = data.get("construction") as Construction;
+    const alchemy = data.get("alchemy") as Alchemy;
 
     (collider.atoms[0] as HydrogenAtom).daysSinceUpgrade = optLacc[134];
     (collider.atoms[5] as CarbonAtom).wizardTowersOver50 = construction.buildings.slice(9, 18).reduce((sum, tower) => sum += Math.max(0, tower.level - 50), 0);
     (collider.atoms[8] as FluorideAtom).voidMeals = cooking.meals.reduce((count, meal) => count += meal.level >= 30 ? 1 : 0, 0);
+
+    const colliderBuildingLevel = construction.buildings.find(building => building.name == "Atom Collider")?.level ?? 0;
+    const nenoBonus = collider.atoms[9].getBonus();
+    const bubbleBonusY5 = alchemy.getBubbleBonusForKey("Y5");
+
+    collider.atoms.forEach(atom => {
+        atom.bubbleBonus = bubbleBonusY5;
+        atom.colliderBuildingLevel = colliderBuildingLevel;
+        atom.nenoBonus = nenoBonus;
+    })
 
     return collider;
 }
