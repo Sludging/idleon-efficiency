9b2df6f6eaf4a476c8dff0d6adb3fb4dc2743b83
corbeno <crkems@iastate.edu>
2023-01-18 17:01:04 -0600

    forgot to remove accountData

--- data/domain/idleonData.tsx
+++ data/domain/idleonData.tsx
@@ -115,7 +115,7 @@ const keyFunctionMap: Record<string, Function> = {
     },
     "shrines": (doc: Cloudsave, charCount: number) => parseShrines(safeJsonParse(doc, "Shrine", [])),
     "storage": (doc: Cloudsave, accountData: Map<string, any>, allItems: Item[], charCount: number) => parseStorage(doc, accountData.get("playerNames"), allItems, JSON.parse(doc.get("InvStorageUsed"))),
-    "constellations": (doc: Cloudsave, accountData: Map<string, any>, charCount: number) => parseConstellations(safeJsonParse(doc, "SSprog", [])),
+    "constellations": (doc: Cloudsave, charCount: number) => parseConstellations(safeJsonParse(doc, "SSprog", [])),
     "quests": (doc: Cloudsave, accountData: Map<string, any>, allItems: Item[], charCount: number) => parseQuests(doc, accountData, allItems, charCount),
     "refinery": (doc: Cloudsave, charCount: number) => parseRefinery(safeJsonParse(doc, "Refinery", [])),
     "saltLick": (doc: Cloudsave, charCount: number) => parseSaltLick(safeJsonParse(doc, "SaltLick", [])),
