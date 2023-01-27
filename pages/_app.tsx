f811bc9795cc7431f3e5457567ac7abaf05c43e7
Sludgadin <LLJKSom0@gmail.com>
2023-01-27 14:40:42 +1100

    add artifact boost and rabbit star

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -92,7 +92,8 @@ const customTheme = deepMerge(dark, {
       "Legendary": "#e49c5f",
       "blue-1": "#19243A",
       "blue-2": "#283F70",
-      "blue-3": "#6dcdff"
+      "blue-3": "#6dcdff",
+      "gold-1": "#ffc142",
     }
   },
   notification: {
