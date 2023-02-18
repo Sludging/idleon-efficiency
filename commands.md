832f64c9a50d37694c490058db7cee9259407ecf
Sludgadin <LLJKSom0@gmail.com>
2023-02-19 09:55:44 +1100

    some supporting files updates

--- commands.md
+++ commands.md
@@ -13,13 +13,20 @@ To copy models from the Wikibot:
 cp -r ../IdleonWikiBot/exported/ts/* data/domain/
 ```
 
+New Process for new patches:
+```
+1. Update the app on your android device
+2. use adb to connect to your device, something like: `adb connect 192.168.1.111:39323`
+3. use adb to find the location of the apk: `adb shell pm path com.lavaflame.MMO`
+4. use adb to download the apk to your device: `adb pull <path_from_3> assets`
+5. unzip apk `unzip base.apk -d apk`
+6. profit.
+
 When new patch comes out:
 ```
 1. asar extract "/mnt/d/Program Files/Steam/steamapps/common/Legends of Idleon/resources/app.asar" assets
-2. js-beautify assets/distBuild/static/game/Z.js > Z_clean.js
-3. python -m regex_Z
-4. js-beautify Z_clean_parsed.js > Z_clean.js
-5. Go to https://unminify.com/, upload it and download the unminified version.
+2. js-beautify assets/distBuild/static/game/N.js > N_clean.js
+4. Go to https://unminify.com/, upload it and download the unminified version.
 
 OPTIONAL ALTERNATIVE: 
 1. Download Z.js from web: curl -O https://www.legendsofidleon.com/ytGl5oc/Z.js
---
+++ data/image-manipulator.js
@@ -0,0 +1,45 @@
+(async () => {
+    const fs = require('graceful-fs');
+    const PNG = require('pngjs').PNG;
+    const path = require('path');
+    const pixelmatch = require('pixelmatch');
+    const getDirName = require('path').dirname;
+
+    const assetFolder = "../assets/apk/assets/assets/data/";
+    await fs.readdir(assetFolder, {}, async (err, files) => {
+        files.forEach(async file => {
+                if (!file.includes(".png") || file.substring(0,4) == "Plat" || file.includes("bg.png") || file.includes("menuserver")) {
+                    return;
+                }
+
+                try {
+                    const img = PNG.sync.read(fs.readFileSync(`${assetFolder}/${file}`));
+                    const img2 = PNG.sync.read(fs.readFileSync(`icons/assets/data/${file}`));
+                    const { width, height } = img;
+
+                    const res = pixelmatch(img.data, img2.data, null, width, height, { threshold: 0.1 })
+                    if (res != 0) {
+                        console.log(`Images didn't match for: ${file}`);
+                        fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
+                    }
+                }
+                catch (e) {
+                    if (e.message && e.message.includes("Image sizes do not match")) {
+                        console.log(`Images size didn't match for: ${file}`);
+                        fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
+                    }
+                    else {
+                        console.log(`New Icon: ${file}`)
+                        const img = PNG.sync.read(fs.readFileSync(`${assetFolder}/${file}`));
+                        var buffer = PNG.sync.write(img);
+                        await fs.writeFile(`icons/assets/data/${file}`, buffer, function (err) {
+                            if (err) {
+                                console.log(err);
+                                throw err;
+                            }
+                        })
+                    }
+                }
+            })
+        })
+})();
