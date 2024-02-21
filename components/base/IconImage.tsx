7a3e3211e7d664125012e0682a68c7b090807ccf
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-02-21 15:48:52 +0100

    Added an AdaptativeIconImage
    This new Image change it's own size after the image is loaded and we know it's size
    
    If the width and height of the image data are both at 0, then the image use it's original size
    
    If only one of those two is set at a value higher than 0, when the image is loaded the other value will be updated to keep image ratio and respect the initial height or width
    
    For example, if data.height == 35, when the image is loaded the height will be kept at 35, but the width will be calculated from the image original size
    If the image original height is 42 and the original width is 340,  we first calculate the ratio between asked and original height, then multiply original width by it
    So our final rendered image size should be :
    height = 35
    width = (35/42) * 340

--- components/base/IconImage.tsx
+++ components/base/IconImage.tsx
@@ -6,14 +6,14 @@ import { ImageData } from "../../data/domain/imageData"
 
 const BASE_URL = "https://cdn2.idleonefficiency.com/images"
 
-const IconImage = ({ data, scale = 1, style }: { data: ImageData, scale?: number, style?: React.CSSProperties }) => {
+export const IconImage = ({ data, scale = 1, style }: { data: ImageData, scale?: number, style?: React.CSSProperties }) => {
     if (!data.location || data.location == "" || data.location == "Blank") {
         return (
             <Box height={`${data.height * scale}px`} width={`${data.width * scale}px`} />
         );
     }
 
-    const realLocation = `${BASE_URL}/${data.location}.png`
+    const realLocation = `${BASE_URL}/${data.location}.png`;
     return (
         <Box style={style} height={`${data.height * scale}px`} width={`${data.width * scale}px`}>
             <Image
@@ -24,6 +24,50 @@ const IconImage = ({ data, scale = 1, style }: { data: ImageData, scale?: number
                 loading='eager'
                 style={{
                     objectFit: "contain"
+                }} />
+        </Box>
+    );
+}
+
+export const AdaptativeIconImage = ({ data, scale = 1, style }: { data: ImageData, scale?: number, style?: React.CSSProperties }) => {
+    if (!data.location || data.location == "" || data.location == "Blank") {
+        return (
+            <Box height={`${data.height * scale}px`} width={`${data.width * scale}px`} />
+        );
+    }
+
+    const realLocation = `${BASE_URL}/${data.location}.png`;
+    return (
+        <Box style={style} height='auto' width='auto' >
+            <Image
+                src={realLocation}
+                alt={data.location}
+                height={data.height * scale}
+                width={data.width * scale}
+                loading='eager'
+                onLoad={(e: any) => {
+                    let newData = data;
+
+                    if (data.height == 0) {
+                        if(data.width > 0) {
+                            const ratio = data.width / e.target.naturalWidth;
+                            newData.height = e.target.naturalHeight * ratio;
+                        } else {
+                            newData.height = e.target.naturalHeight;
+                        }
+                    }
+                
+                    if (data.width == 0) {
+                        if(data.height > 0) {
+                            const ratio = data.height / e.target.naturalHeight;
+                            newData.width = e.target.naturalWidth * ratio;
+                        } else {
+                            newData.width = e.target.naturalWidth;
+                        }
+                    }
+
+                    e.target.width = newData.width;
+                    e.target.height = newData.height;
                 }} />
         </Box>
     );
