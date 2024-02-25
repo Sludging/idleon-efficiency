b852af11ed75cad0921dbb2d702c31ea252cd7bc
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-02-25 16:26:15 +0100

    Started to modify Summoning page to display the "Loading..."

--- pages/world-6/summoning.tsx
+++ pages/world-6/summoning.tsx
@@ -4,15 +4,29 @@ import {
     Text,
 } from 'grommet'
 import { NextSeo } from 'next-seo';
+import { useContext, useState } from 'react';
+import { AppContext } from '../../data/appContext';
+import { SummonEssenceColor, Summoning as SummoningDomain } from '../../data/domain/world-6/summoning';
+import { Player } from '../../data/domain/player';
+import IconImage from '../../components/base/IconImage';
 
 function Summoning() {
-    return (
-        <Box>
-            <NextSeo title="Summoning" />
-            <Heading level="2" size="medium" style={{ fontWeight: 'normal' }}>Summoning</Heading>
-            <Text size="xsmall">* This is a work in progress, there could some bugs and minor inaccuracies. THE UI ISN&apos;T FINAL!</Text>
-        </Box>
-    )
+    const appContext = useContext(AppContext);
+    const data = appContext.data.getData();
+
+    const summoning = data.get("summoning") as SummoningDomain;
+
+    if (!summoning) {
+        return <>Loading...</>
+    } else {
+        return (
+            <Box>
+                <NextSeo title="Summoning" />
+                <Heading level="2" size="medium" style={{ fontWeight: 'normal' }}>Summoning</Heading>
+                <Text size="medium">* This is a work in progress, there could some bugs and minor inaccuracies. THE UI ISN&apos;T FINAL!</Text>
+            </Box>
+        )
+    }
 }
 
 export default Summoning;
