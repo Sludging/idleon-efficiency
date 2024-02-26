a843f1408a7f93aea7aa60bab5f83bd97e0472dd
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-02-26 22:45:12 +0100

    Made small ajustements to display of Upgrades

--- components/world-6/summoning/summoningUpgrades.tsx
+++ components/world-6/summoning/summoningUpgrades.tsx
@@ -5,52 +5,51 @@ import TextAndLabel, { ComponentAndLabel } from "../../base/TextAndLabel";
 import { nFormatter } from "../../../data/utility";
 import { Summoning as SummoningDomain, SummonUpgrade, SummonEssence, SummonEssenceColor } from '../../../data/domain/world-6/summoning';
 
-const ColorSection = ({ upgrades, essence} : { upgrades: SummonUpgrade[], essence: SummonEssence } ) => {
+const ColorSection = ({ upgrades, essence, index }: { upgrades: SummonUpgrade[], essence: SummonEssence, index: number }) => {
+    const borderColor = index > 0 ? "grey-1" : "none";
     return (
-        <Box direction="column">
-            <Grid columns={{ size: 'medium', count: 'fill' }} fill>
-                {
-                    upgrades
-                    .map((upgrade, index) => {
-                        const canAfford = essence.quantity > upgrade.nextLevelCost();
-                        return (
-                            <ShadowBox style={{ opacity: upgrade.level > 0 ? 1 : 0.6 }} key={index} background="dark-1" margin={{ right: 'small', bottom: 'small' }} pad="medium" gap="medium">
-                                <Box direction="column" gap="small">
-                                    <Box direction="row" gap="small" align="center">
-                                        <Stack>
-                                            <Box>
-                                                <IconImage data={upgrade.getBorderImageData()} />
-                                            </Box>
-                                            <Box>
-                                                <IconImage data={upgrade.getImageData()} />
-                                            </Box>                                                
-                                        </Stack>     
-                                        <Text size="small">{upgrade.data.name}{" (" + upgrade.getLevelDisplay() + ")"}</Text>
-                                    </Box>
-                                    <TextAndLabel textSize='xsmall' text={upgrade.getBonusText()} label={"Bonus"} />
-                                    {
-                                        <ComponentAndLabel
-                                            label={upgrade.level > 0 ? "Next level cost" : "Unlock Cost"}
-                                            component={
-                                                upgrade.level < upgrade.data.maxLvl ?
-                                                <Box gap="xsmall" direction="row" align="center">
-                                                    <IconImage data={SummoningDomain.getEssenceIcon(essence.color)} />
-                                                    <Text color={canAfford ? 'green-1' : ''} size="small">{nFormatter(upgrade.nextLevelCost())}</Text>
-                                                </Box>
-                                                :
-                                                <Box gap="xsmall" direction="row" align="center">
-                                                    <Text size="small">MAXED</Text>
-                                                </Box>
-                                            }
-                                        />
-                                    }
+        <Grid columns={{ size: 'medium', count: 'fill' }} pad="small" border={{ side: 'top', color: borderColor }} fill>
+            {
+                upgrades
+                .map((upgrade, index) => {
+                    const canAfford = essence.quantity > upgrade.nextLevelCost();
+                    return (
+                        <ShadowBox style={{ opacity: upgrade.level > 0 ? 1 : 0.6 }} key={index} background="dark-1" margin={{ right: 'small', bottom: 'small' }} pad="medium" gap="medium">
+                            <Box direction="column" gap="small">
+                                <Box direction="row" gap="small" align="center">
+                                    <Stack>
+                                        <Box>
+                                            <IconImage data={upgrade.getBorderImageData()} />
+                                        </Box>
+                                        <Box>
+                                            <IconImage data={upgrade.getImageData()} />
+                                        </Box>                                                
+                                    </Stack>     
+                                    <Text size="small">{upgrade.data.name}{" (" + upgrade.getLevelDisplay() + ")"}</Text>
                                 </Box>
-                            </ShadowBox>
-                        )
-                    })
-                }
-            </Grid>
-        </Box>
+                                <TextAndLabel textSize='xsmall' text={upgrade.getBonusText()} label={"Bonus"} />
+                                {
+                                    <ComponentAndLabel
+                                        label={upgrade.level > 0 ? "Next level cost" : "Unlock Cost"}
+                                        component={
+                                            upgrade.level < upgrade.data.maxLvl ?
+                                            <Box gap="xsmall" direction="row" align="center">
+                                                <IconImage data={SummoningDomain.getEssenceIcon(essence.color)} />
+                                                <Text color={canAfford ? 'green-1' : ''} size="small">{nFormatter(upgrade.nextLevelCost())}</Text>
+                                            </Box>
+                                            :
+                                            <Box gap="xsmall" direction="row" align="center">
+                                                <Text size="small">MAXED</Text>
+                                            </Box>
+                                        }
+                                    />
+                                }
+                            </Box>
+                        </ShadowBox>
+                    )
+                })
+            }
+        </Grid>
     )
 }
 
@@ -66,9 +65,9 @@ export const SummoningUpgrades = ({ upgrades, essences } : { upgrades: SummonUpg
                 <Box direction="column" margin={{ top: 'small' }}>
                     {
                         essences
-                        .filter(essence => essence.display == true)
-                        .map((essence, index) => (
-                            <ColorSection key={index} upgrades={upgradesToDisplay.filter(upgrade => upgrade.data.colour == essence.color)} essence={essence} />
+                            .filter(essence => essence.display == true)
+                            .map((essence, index) => (
+                                <ColorSection key={index} index={index} upgrades={upgradesToDisplay.filter(upgrade => upgrade.data.colour == essence.color)} essence={essence} />
                         ))
                     }
                 </Box>
