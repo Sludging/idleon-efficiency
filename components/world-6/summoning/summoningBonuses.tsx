424d763ce049724b17b3257924b3286e0093cd76
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-02-26 18:24:40 +0100

    Created empty component for summoning bonuses

---
+++ components/world-6/summoning/summoningBonuses.tsx
@@ -0,0 +1,12 @@
+import { Box, Grid, Text } from "grommet";
+import ShadowBox from "../../base/ShadowBox";
+import IconImage from "../../base/IconImage";
+import TextAndLabel, { ComponentAndLabel } from "../../base/TextAndLabel";
+import { nFormatter } from "../../../data/utility";
+import { Summoning, SummonBonus } from "../../../data/domain/world-6/summoning";
+
+export const SummoningBonuses = ({ bonuses } : { bonuses: SummonBonus[] } ) => {
+    return (
+        <Text>Bonuses</Text>
+    )
+}
