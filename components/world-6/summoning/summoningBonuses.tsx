f73650dfea545570ea0e581b4c034ce753255be6
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-02-27 00:00:39 +0100

    Basic display for Summoning bonuses

--- components/world-6/summoning/summoningBonuses.tsx
+++ components/world-6/summoning/summoningBonuses.tsx
@@ -5,8 +5,22 @@ import TextAndLabel, { ComponentAndLabel } from "../../base/TextAndLabel";
 import { nFormatter } from "../../../data/utility";
 import { Summoning, SummonBonus } from "../../../data/domain/world-6/summoning";
 
-export const SummoningBonuses = ({ bonuses } : { bonuses: SummonBonus[] } ) => {
-    return (
-        <Text>Bonuses</Text>
-    )
+export const SummoningBonuses = ({ bonuses }: { bonuses: SummonBonus[] }) => {
+    if (bonuses.length == 0) {
+        return <Text>You should start by winning some battles in W6 town first</Text>
+    } else {
+        return (
+            <Grid columns={{ size: 'auto', count: 4 }} margin={{top:"small"}} fill>
+                {
+                    bonuses.map((bonus, index) => {
+                        return (
+                            <ShadowBox style={{ opacity: bonus.bonusValue > 0 ? 1 : 0.6 }} key={index} background="dark-1" margin={{ right: 'small', bottom: 'small' }} pad="medium" gap="medium">
+                                <Text size="small">{bonus.getBonusText()}</Text>
+                            </ShadowBox>
+                        )
+                    })
+                }
+            </Grid>
+        )
+    }
 }
