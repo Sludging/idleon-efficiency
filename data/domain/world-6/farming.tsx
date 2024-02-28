6c307fe037950199473746ecfbdca769b1ab9319
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-02-28 18:49:54 +0100

    Start of supporting Farming
    Still need to figure out how to get some data

--- data/domain/world-6/farming.tsx
+++ data/domain/world-6/farming.tsx
@@ -1,15 +1,108 @@
+import { SkillsIndex } from "../SkillsIndex";
 import { Domain, RawData } from "../base/domain";
+import { initMarketInfoRepo } from "../data/MarketInfoRepo";
+import { SeedInfoBase, initSeedInfoRepo } from "../data/SeedInfoRepo";
 import { Item } from "../items";
+import { MarketInfoModel } from "../model/marketInfoModel";
+import { SeedInfoModel } from "../model/seedInfoModel";
+import { Player } from "../player";
+
+export class MarketUpgrade {
+    level: number = 0;
+
+    constructor(public index: number, public data: MarketInfoModel) {}
+}
+
+export class Crop {
+    constructor(public index: number, public quantityOwned: number, public discovered: boolean) {}
+}
+
+export class Seed {
+    constructor(public index: number, public data: SeedInfoModel) {}
+}
+
+export class Plot {
+    // -1 means the Plot is empty
+    seedIndex: number = -1;
+    cropIndex: number = 0;
+    // If true can't evolve, but can still OverGrow
+    locked: boolean = false;
+    // Time in seconds since last Collecting or Planting
+    secondesSinceLastInteraction: number = 0;
+
+    toDefine1: number = 0;
+    toDefine2: number = 0;
+    toDefine3: number = 0;
+
+    constructor(public index: number) {}
+}
 
 export class Farming extends Domain {
+    farmPlots: Plot[] = [];
+    marketUpgrades: MarketUpgrade[] = initMarketInfoRepo().map((upgrade) => { return new MarketUpgrade(upgrade.index, upgrade.data) });
+    seeds: Seed[] = initSeedInfoRepo().map((seed) => { return new Seed(seed.index, seed.data) });;
+    cropDepot: Crop[] = [];
+    farmingLevel: number = 0;
+
     getRawKeys(): RawData[] {
-        throw new Error("Method not implemented.");
+        return [
+            { key: "FarmPlot", perPlayer: false, default: []},
+            { key: "FarmCrop", perPlayer: false, default: []},
+            { key: "FarmUpg", perPlayer: false, default: []},
+        ]
     }
+
     init(allItems: Item[], charCount: number) {
-        throw new Error("Method not implemented.");
+        return this;
     }
+
     parse(data: Map<string, any>): void {
-        throw new Error("Method not implemented.");
+        const farming = data.get(this.dataKey) as Farming;
+        const cropsData = data.get("FarmCrop") as Object;
+        const plotsData = data.get("FarmPlot") as number[][];
+        const upgradesData = data.get("FarmUpg") as number[];
+
+        // TODO : extract owned crops info from this
+        // The number of values <index:quantity> in this is equal to foundCrops
+        //console.log("Crops :");
+        //console.log(cropsData);
+
+        farming.marketUpgrades.forEach((upgrade) => {
+            if (upgrade.index < upgradesData.length) {
+                upgrade.level = upgradesData[upgrade.index];
+            }
+        });
+
+        
+        farming.farmPlots = [];
+        //console.log("Plots :");
+        plotsData.forEach((plotInfo, index) => {
+            //console.log(index + " : " + plotInfo);
+            let plot: Plot = new Plot(index);
+            plot.seedIndex = plotInfo[0];
+            // If seedIndex = -1 then the plot is empty, so no more information are needed
+            if(plot.seedIndex > -1) {
+                plot.secondesSinceLastInteraction = plotInfo[1];
+                plot.cropIndex = plotInfo[2] + (farming.seeds.find(seed => seed.index == plot.seedIndex)?.data.cropIdMin ?? 0);
+                plot.locked = (plotInfo[3] == 1);
+
+                // TODO : figure out the last 3 (might be OG, evolution or something like that, will need to wait for my crops to grow to check)
+                plot.toDefine1 = plotInfo[4];
+                plot.toDefine2 = plotInfo[5];
+                plot.toDefine3 = plotInfo[6];
+            }
+            farming.farmPlots.push(plot);
+        });
     }
-    
+
+    getDiscoveredCropsNumber = (): number => {
+        return this.cropDepot.filter(crop => crop.discovered == true).length;
+    }
+}
+
+export const updateFarmingLevel = (data: Map<string, any>) => {
+    const farming = data.get("farming") as Farming;
+    const players = data.get("players") as Player[];
+
+    farming.farmingLevel = players[0]?.skills.get(SkillsIndex.Farming)?.level ?? 0;
 }
