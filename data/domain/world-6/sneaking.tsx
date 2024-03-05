ff45d97c01059712f04d63a91951348869f68a6d
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-03-05 18:19:10 +0100

    Sneaking items are now in class

--- data/domain/world-6/sneaking.tsx
+++ data/domain/world-6/sneaking.tsx
@@ -9,8 +9,12 @@ import { NinjaItemTypeEnum } from "../enum/ninjaItemTypeEnum";
 import { ImageData } from "../imageData";
 import { Item } from "../items";
 import { JadeUpgradeModel } from "../model/jadeUpgradeModel";
-import { NinjaPristineCharmModel } from "../model/ninjaPristineCharmModel";
+import { NinjaPristineCharmModel } from '../model/ninjaPristineCharmModel';
 import { NinjaUpgradeModel } from "../model/ninjaUpgradeModel";
+import { NinjaItemModel } from '../model/ninjaItemModel';
+import { NinjaWeaponModel } from '../model/ninjaWeaponModel';
+import { NinjaTrinketModel } from '../model/ninjaTrinketModel';
+import { BaseNinjaItemModel } from "../model/baseNinjaItemModel";
 
 export enum SneakingActivity {
     Sneaking = "Sneaking",
@@ -20,34 +24,136 @@ export enum SneakingActivity {
     KnockedOut = "Knocked Out",
 }
 
-export interface SneakingEquipment {
-    name: string
-    level: number
+export enum SneakinWeaponType {
+    Nunchaku = 1,
+    Gloves = 0,
+    Kunai = 2
+}
+
+export const DoorsMaxHP = [
+    1,
+    150,
+    1000,
+    8000,
+    45000,
+    350000,
+    5000000,
+    80000000,
+    1000000000,
+    25000000000.0,
+    800000000000.0,
+    45000000000000.0
+]
+
+export const TiedPlayersHP = [
+    0,
+    10,
+    60,
+    750,
+    3000,
+    17000,
+    80000,
+    500000,
+    1500000,
+    7000000,
+    30000000,
+    200000000
+]
+
+export interface SneakingDoor {
+    damageDone: number,
+    maxHP: number
+}
+
+export interface SneakingTie {
+    damageDone: number,
+    maxHP: number
+}
+
+export class SneakingItem {
+    constructor(public index: number, public data: BaseNinjaItemModel) {}
+
+    getImageData = (): ImageData => {
+        return {
+            location: `${this.data.internalId}`,
+            height: 60,
+            width: 60
+        }
+    }
+
+    static fromBase(baseItem: BaseNinjaItemBase | undefined, level: number = 0): SneakingItem | undefined {
+        if (baseItem) {
+            if (baseItem.data.internalId == "Blank") {
+                return undefined;
+            }
+            switch (baseItem.data.itemType) {
+                case NinjaItemTypeEnum.Item:
+                    return new SneakingHat(baseItem.index, baseItem.data as NinjaItemModel);
+                case NinjaItemTypeEnum.Weapon:
+                    return new SneakingWeapon(baseItem.index, baseItem.data as NinjaWeaponModel, level);
+                case NinjaItemTypeEnum.Trinket:
+                    return new SneakingTrinket(baseItem.index, baseItem.data as NinjaTrinketModel, level);
+                case NinjaItemTypeEnum.PristineCharm:
+                    return new SneakingPristineCharm(baseItem.index, baseItem.data as NinjaPristineCharmModel);
+                default:
+                    return undefined;
+            }
+        } else {
+            return undefined;
+        }
+    }
+}
+
+export class SneakingHat extends SneakingItem {
+    constructor(public index: number, public data: NinjaItemModel) {
+        super(index, data);
+    }
+}
+
+export class SneakingWeapon extends SneakingItem {
+    constructor(public index: number, public data: NinjaWeaponModel, public level: number) {
+        super(index, data);
+    }
+}
+
+export class SneakingTrinket extends SneakingItem {
+    constructor(public index: number, public data: NinjaTrinketModel, public level: number) {
+        super(index, data);
+    }
+}
+
+export class SneakingPristineCharm extends SneakingItem {
+    constructor(public index: number, public data: NinjaPristineCharmModel) {
+        super(index, data);
+    }
+}
+
+export class PlayerEquipment {
+    constructor(public hat: SneakingItem | undefined, public weapon: SneakingWeapon | undefined, public Trinket1: SneakingTrinket | undefined, public Trinket2: SneakingTrinket | undefined) {}
 }
 
 export class SneakingPlayer {
     activity: SneakingActivity = SneakingActivity.Tied;
-    equipment: SneakingEquipment[] = [];
+    equipment: PlayerEquipment = new PlayerEquipment(undefined, undefined, undefined, undefined);
     // The action timer is based on this, action time - time past will tell you how long till next action calculation.
     timePast: number = 0;
+
+    tie: SneakingTie = {damageDone: 0, maxHP: 0};
 
     floor: number = 9;
     // Temp holding of raw info so it's easier to map.
     rawData: number[];
-    constructor(public index: number, public level: number, activityInfo: number[], equipment: SneakingEquipment[]) {
+    constructor(public index: number, public level: number, activityInfo: number[], equipment: PlayerEquipment) {
         this.rawData = activityInfo;
         this.floor = activityInfo[0];
         // activityInfo[1] can be one of two things:
         // Positive -> Seconds spent actively doing a task (could be sneaking, untying or breaching)
         // Negative -> Seconds spent being down
-        if (activityInfo[1] != 0) {
-            // TODO: Make this smarter, probably based on equipment?
-            this.activity = SneakingActivity.Sneaking;
-        }
-        if (activityInfo[1] < 0) {
-            this.activity = SneakingActivity.KnockedOut;
-        }
         this.equipment = equipment;
+        this.tie.damageDone = activityInfo[3];
+        if (this.index < TiedPlayersHP.length) {
+            this.tie.maxHP = TiedPlayersHP[this.index];
+        }
     }
 }
 
@@ -158,9 +264,10 @@ export class PristineCharm {
 }
 
 export class Sneaking extends Domain {
-    inventory: SneakingEquipment[] = [];
+    inventory: (SneakingItem | undefined)[] = [];
     players: SneakingPlayer[] = [];
     pristineCharms: PristineCharm[] = [];
+    doors: SneakingDoor[] = [];
     jade: number = 0;
 
     // Store the item source data so we can make decisions based on it.
@@ -220,15 +327,22 @@ export class Sneaking extends Domain {
             const sneakingLevel = playerLevel[SkillsIndex.Sneaking];
             // Equipment per player is stored in 4 different indexes, starting from 10.
             // Figure how the starting index of current player.
-            const startingIndex = 10 + 4 * index;
+            const startingIndex = 12 + 4 * index;
             // get 4 indexes from the array starting from starting index.
-            const playerEquipment = ninjaData.slice(startingIndex, startingIndex + 4)
-            sneaking.players.push(new SneakingPlayer(index, sneakingLevel, playerInfo, playerEquipment))
+            const playerEquipment = ninjaData.slice(startingIndex, startingIndex + 4);
+            let hat: SneakingHat | undefined = SneakingItem.fromBase(sneaking.baseItems.find(item => item.data.internalId == playerEquipment[0][0]), playerEquipment[0][1]) as SneakingHat | undefined;
+            let weapon: SneakingWeapon | undefined = SneakingItem.fromBase(sneaking.baseItems.find(item => item.data.internalId == playerEquipment[1][0]), playerEquipment[1][1]) as SneakingWeapon | undefined;
+            let trinket1: SneakingTrinket | undefined = SneakingItem.fromBase(sneaking.baseItems.find(item => item.data.internalId == playerEquipment[2][0]), playerEquipment[2][1]) as SneakingTrinket | undefined;
+            let trinket2: SneakingTrinket | undefined = SneakingItem.fromBase(sneaking.baseItems.find(item => item.data.internalId == playerEquipment[3][0]), playerEquipment[3][1]) as SneakingTrinket | undefined;
+
+            sneaking.players.push(new SneakingPlayer(index, sneakingLevel, playerInfo, new PlayerEquipment(hat, weapon, trinket1, trinket2)));
         }) 
 
-        ninjaData.slice(60,99).forEach((equipment: {name: string, level: number}) => {
-            if(equipment.name != "Blank") {
-                sneaking.inventory.push({name: equipment.name, level: equipment.level});
+        ninjaData.slice(60,99).forEach((equipment: [name: string, level: number]) => {
+            if (equipment[0] != "Blank") {
+                sneaking.inventory.push(SneakingItem.fromBase(sneaking.baseItems.find(item => item.data.internalId == equipment[0]), equipment[1]));
+            } else {
+                sneaking.inventory.push(undefined);
             }
         });
 
@@ -262,6 +376,29 @@ export class Sneaking extends Domain {
             upgrade.purchased = purchasedUpgrades.includes(upgrade.index);
             upgrade.costExponent = serverVars["A_empoExpon"];
         })
+
+        sneaking.doors = [];
+        const doorsDamage = ninjaData[100] as number[];
+        doorsDamage.forEach((damageDone, index) => {
+            sneaking.doors.push({damageDone: damageDone, maxHP: ((index < DoorsMaxHP.length) ? DoorsMaxHP[index] : 1E9999999999)});
+        });
+    }
+
+    updatePlayersActivity = () => {
+        this.players.forEach((player, index) => {
+            const currentFloorDoor = (player.floor < this.doors.length) ? this.doors[player.floor] : undefined;
+    
+            if (player.tie.damageDone < player.tie.maxHP || player.rawData[1] == 0) {
+                player.activity = SneakingActivity.Tied
+            } else if (player.rawData[1] < 0) {
+                player.activity = SneakingActivity.KnockedOut;
+            } else if (player.rawData[1] > 0) {
+                switch (true) {
+                    default:
+                        player.activity = SneakingActivity.Sneaking;
+                }
+            }
+        })
     }
 }
 
@@ -273,4 +410,6 @@ export const updateSneaking = (data: Map<string, any>) => {
     if (currencyConduitUpgrade) {
         currencyConduitUpgrade.bubbleDiscount = alchemy.getBonusForBubble(CauldronIndex.Kazam, AlchemyConst.LoCostMoJade);
     }
+
+    sneaking.updatePlayersActivity();
 }
