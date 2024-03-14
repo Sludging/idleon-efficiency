71881f4029a036a743426dcc44c8f5d216416976
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-03-14 23:27:08 +0100

    Forgot to save the file to put it into last commit

--- pages/world-6/sneaking.tsx
+++ pages/world-6/sneaking.tsx
@@ -62,7 +62,7 @@ function Sneaking() {
                 <PlayerActivitySection sneakingPlayers={sneaking.players} players={players} />
             </Box>
             <Box align="center" direction="row" justify="center" gap="small">
-                {["Jade Upgrades", "Sneaking Upgrades", "Inventory", "Pristine Charms", (sneaking.beanstalking.unlocked ? "Beanstalck" : "")].filter(tab => tab != "").map((tabName, index) => (
+                {["Jade Upgrades", "Sneaking Upgrades", "Inventory", "Pristine Charms", (sneaking.beanstalking.unlocked ? "Beanstalk" : "")].filter(tab => tab != "").map((tabName, index) => (
                     <TabButton key={index} isActive={activeTab == tabName} text={tabName} clickHandler={() => { setActiveTab(tabName); }} />
                 ))
                 }
@@ -71,7 +71,7 @@ function Sneaking() {
             {activeTab == "Sneaking Upgrades" && <SneakingUpgrades currentJade={sneaking.jade} upgrades={sneaking.sneakingUpgrades} />}
             {activeTab == "Inventory" && <SneakingInventory inventory={sneaking.inventory} />}
             {activeTab == "Pristine Charms" && <PristineCharmSection charms={sneaking.pristineCharms} />}
-            {activeTab == "Beanstalck" && <BeanstalkingDisplay beanstalking={sneaking.beanstalking} />}
+            {activeTab == "Beanstalk" && <BeanstalkingDisplay beanstalking={sneaking.beanstalking} />}
         </Box>
     )
 }
