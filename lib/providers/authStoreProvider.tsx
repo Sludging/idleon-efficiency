31a1528d25beccc22800fc2834530b43fc7e5fd8
Someone <Someone@somewhere.com>
2024-03-19 14:01:27 +0530

    adding some logs to debug

--- lib/providers/authStoreProvider.tsx
+++ lib/providers/authStoreProvider.tsx
@@ -40,6 +40,7 @@ export const useAuthStore = <T,>(
   // Probably a better way to handle this, but it works for now.
   const currentState = authStoreContext.getState()
   if (!currentState.initialized) {
+    console.log("Calling init");
     currentState.initialize();
   }
 
