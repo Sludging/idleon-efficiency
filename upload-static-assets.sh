3c49259560cd858c6ebfccdf8ac7a23dcf070ab1
Someone <Someone@somewhere.com>
2024-03-19 11:11:19 +0530

    use preview specific subfolder

--- upload-static-assets.sh
+++ upload-static-assets.sh
@@ -6,11 +6,15 @@ CURRENT_PATCH=$(grep -P -o '^const currentPatch = "\K[a-zA-Z0-9\.-]*' next.confi
 if [ "$NODE_ENV" = "production" ] || [ "$NODE_ENV" = "preview" ]; then
     echo "Uploading latest build files to idleon-efficiency-images/${CURRENT_PATCH}/_next/static"
 
-    # This should always be executed from the root of the repo, or the pathing won't be correct.
     MAX_AGE=2592000
+    # In preview we want to have a shorter time for refetching,
+    # so use shorter max age and use a preview specific folder
     if [ "$NODE_ENV" = "preview" ]; then
         MAX_AGE=300
+        CURRENT_PATCH="${CURRENT_PATCH}-preview"
     fi
+
+    # This should always be executed from the root of the repo, or the pathing won't be correct.
     aws s3 sync .next/static s3://idleon-efficiency-images/${CURRENT_PATCH}/_next/static --acl bucket-owner-full-control --cache-control max-age=${MAX_AGE},public --expires 2044-01-01T00:00:00Z
 else
     echo "Skipping upload, not in production (${NODE_ENV})"
