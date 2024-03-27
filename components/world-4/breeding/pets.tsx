63974b936366233894b15e18ef83ee9640fa74af
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-03-27 14:48:54 +0100

    Created Breedability display
    Copied from Shiny and adapted it, removed filter as every mob have the same bonus

--- components/world-4/breeding/pets.tsx
+++ components/world-4/breeding/pets.tsx
@@ -30,13 +30,9 @@ export const PetsDisplay = () => {
             </Box>
             {activeTab == "All" && <PetDisplay />}
             {activeTab == "Shiny" && <ShinyDisplay />}
-            {activeTab == "Breedability" && <ShinyDisplay />}
+            {activeTab == "Breedability" && <BreedabilityDisplay />}
         </Box>
     )
-}
-
-function BreedabilityDisplay() {
-
 }
 
 function PetDisplay() {
@@ -122,7 +118,9 @@ function ShinyDisplay() {
     const size = useContext(ResponsiveContext);
 
     // Get breeding data, if it's not available yet just show placeholder loading.
-    const breeding = appContext.data.getData().get("breeding") as BreedingDomain;
+    const breeding = useMemo(() => {
+        return appContext.data.getData().get("breeding") as BreedingDomain;
+    }, [appContext])
 
     // our sort options are fixed, so just statically set them.
     const sortOptions = ["Level", "Least Time to Next Level"];
@@ -164,11 +162,11 @@ function ShinyDisplay() {
             // Least time to next level sort function
             function sortByTime(pet1: Pet, pet2: Pet) {
                 // If pet is max level, move it to the end
-                if (pet1.calculateShinyLevel() >= 20) {
+                if (pet1.shinyLevel >= 20) {
                     return -1;
                 }
                 // If pet is max level, move it to the end
-                if (pet2.calculateShinyLevel() >= 20) {
+                if (pet2.shinyLevel >= 20) {
                     return -1;
                 }
                 // Else sort by time till next level.
@@ -177,12 +175,12 @@ function ShinyDisplay() {
 
             // Sort by level sort function
             function sortByLevel(pet1: Pet, pet2: Pet) {
-                if (pet1.calculateShinyLevel() == pet2.calculateShinyLevel()) {
+                if (pet1.shinyLevel == pet2.shinyLevel) {
                     // If level is equal, sort by time until next level
                     return sortByTime(pet1, pet2);
                 } else {
                     // if level isn't equal, just sort by level
-                    return pet1.calculateShinyLevel() > pet2.calculateShinyLevel() ? -1 : 1;
+                    return pet1.shinyLevel > pet2.shinyLevel ? -1 : 1;
                 }
             }
 
@@ -253,6 +251,106 @@ function ShinyDisplay() {
                                         <Text size="16px">{Math.floor(pet.shinyProgress)}/{pet.getNextShinyGoal()} days</Text>
                                     </Box>
                                     <Text size="16px">{pet.getShinyText()}</Text>
+                                </Grid>
+                            </ShadowBox>
+                        )
+                    })
+                }
+            </Grid>
+        </Box>
+    )
+}
+
+function BreedabilityDisplay() {
+    const [sort, setSort] = useState<string>('');
+    const appContext = useContext(AppContext);
+    const size = useContext(ResponsiveContext);
+
+    // Get breeding data, if it's not available yet just show placeholder loading.
+    const breeding = useMemo(() => {
+        return appContext.data.getData().get("breeding") as BreedingDomain;
+    }, [appContext])
+
+    // our sort options are fixed, so just statically set them.
+    const sortOptions = ["Level", "Least Time to Next Level"];
+
+    const petsToShow = useMemo(() => {
+        // If we are still loading, do nothing.
+        if (!breeding) {
+            return [];
+        }
+        // Start with a base list of all pets
+        let pets: Pet[] = breeding.basePets.filter(pet => pet.data.petId != "_");
+
+        // Now we sort
+        return pets?.sort((pet1, pet2) => {
+            // Base case scenario they are just by index.
+            const indexSort = pet1.index > pet2.index ? 1 : -1;
+
+            // Least time to next level sort function
+            function sortByTime(pet1: Pet, pet2: Pet) {
+                // Else sort by time till next level.
+                return (pet1.getNextBreedingGoal() - pet1.breedingProgress) > (pet2.getNextBreedingGoal() - pet2.breedingProgress) ? 1 : -1;
+            }
+
+            // Sort by level sort function
+            function sortByLevel(pet1: Pet, pet2: Pet) {
+                if (pet1.breedingLevel == pet2.breedingLevel) {
+                    // If level is equal, sort by time until next level
+                    return sortByTime(pet1, pet2);
+                } else {
+                    // if level isn't equal, just sort by level
+                    return pet1.breedingLevel > pet2.breedingLevel ? -1 : 1;
+                }
+            }
+
+            switch (sort) {
+                case "Level":
+                    return sortByLevel(pet1, pet2);
+                case "Least Time to Next Level":
+                    return sortByTime(pet1, pet2);
+                default:
+                    return indexSort;
+            }
+        })
+    }, [breeding, sort])
+
+    if (!breeding) {
+        return (
+            <Box>
+                Still loading
+            </Box>
+        )
+    }
+
+    return (
+        <Box gap="small">
+            <Text>Breedability</Text>
+            <Box direction="row" gap="medium" margin={{bottom: "small"}}>
+                <Select size="small"
+                    placeholder="Sort by"
+                    clear
+                    value={sort}
+                    options={sortOptions}
+                    onChange={({ value: nextValue }) => { setSort(nextValue); }}
+                />
+            </Box>
+            <Grid columns={size == "small" ? ["1"] : ["1/3", "1/3", "1/3"]} fill>
+                {
+                    petsToShow?.filter(pet => pet.data.petId != "_").map((pet, pIndex) => {
+                        const petInFenceyard: boolean = breeding.fenceyardPets.some(fenceyardPet => fenceyardPet.data.petId == pet.data.petId && fenceyardPet.gene.index == 4);
+                        const currentlyLevelingBreedingBorderProp: BorderType = petInFenceyard && { size: '2px', style: 'solid', color: 'green' };
+                        const enemy = EnemyInfo.find(enemy => enemy.id == pet.data.petId);
+
+                        return (
+                            <ShadowBox background="dark-1" border={currentlyLevelingBreedingBorderProp} key={pIndex} direction="row" gap="medium" margin={{ bottom: 'medium', right: 'small' }} align="center" pad="small" style={{ opacity: pet.breedingLevel > 0 ? 1 : .5 }}>
+                                <IconImage data={{ location: enemy?.id.toLowerCase() ?? "Unknown", width: 67, height: 67 }} style={{ paddingBottom: '15px' }} />
+                                <Grid columns={["50%", "50%"]} fill align="center">
+                                    <Box>
+                                        <Text size="16px">Lvl: {pet.breedingLevel}</Text>
+                                        <Text size="16px">{Math.floor(pet.breedingProgress)}/{pet.getNextBreedingGoal()} days</Text>
+                                    </Box>
+                                    <Text size="16px">x{nFormatter(pet.getBreedabilityBonus())}</Text>
                                 </Grid>
                             </ShadowBox>
                         )
