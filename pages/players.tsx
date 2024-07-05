283093473adefde08a51c260bcf80c517851fefc
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-07-05 10:00:27 +0200

    Updated maps to ignore for Zow/Chow
    Added back the W5 boss map
    
    Added the new zones for the Howl and Poppy mini-games

--- pages/players.tsx
+++ pages/players.tsx
@@ -1059,9 +1059,11 @@ function ZowInfo({ player }: { player: Player }) {
             "Spike Surprise", "YumYum Grotto", "Salty Shores", "Faraway Piers", "Filler", "Deepwater Docks", "Bandit Bob's Hideout", "Frostbite Towndra",
             "Tunnels Entrance", "Trappers Folley", "Freefall Caverns", "The Ol' Straightaway", "Slip Slidy Ledges", "Echoing Egress",
             "Blunder Hills", "JungleZ", "PlayerSelect", "Efaunt's Tomb", "Gravel Tomb", "Heaty Hole", "End Of The Road", "Z", "Eycicles's Nest", "Enclave a la Troll",
-            "Magma Rivertown", "YumYum Islands", "Chizoar's Cavern", "Tunnel Closed",
+            "Magma Rivertown", "YumYum Islands", "Chizoar's Cavern", "Tunnel Closed", "KattleKruk's Volcano",
             // world 6
-            "Motherlode Pit", "Companion Park", "Spirit Village", "Castle Interior"
+            "Motherlode Pit", "Companion Park", "Spirit Village", "Castle Interior",
+            // minigame Howl and Poppy
+            "Grand Owl Perch", "The Oasis"
         ].includes(area)) {
             return true;
         }
