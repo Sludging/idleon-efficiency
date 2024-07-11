aaab5f2d9394b12973f5d2256074935852f0efdf
Sludgadin <LLJKSom0@gmail.com>
2024-07-11 23:30:30 +1000

    fix players mistake

--- app/players/content.tsx
+++ app/players/content.tsx
@@ -198,7 +198,7 @@ function ShowSkills(props: SkillProps) {
 
 function MiscStats({ player, activeBubbles }: { player: Player, activeBubbles: Bubble[] }) {
     const { theData } = useAppDataStore(useShallow(
-        (state) => ({ theData: state.data.getData(), lastUpdated: state.lastUpdated })
+        (state) => ({ theData: state.data.getData(), lastUpdated: state.data.getLastUpdated(true) as Date | undefined, realLastUpdated: state.lastUpdated })
     ));
     const size = useContext(ResponsiveContext)
 
