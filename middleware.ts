75c1101bf342693fa41182d5be62435cfeb91185
Sludgadin <LLJKSom0@gmail.com>
2024-07-26 19:12:46 +1000

    try more headers

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,9 @@ export function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
     // No custom logic, do this for EVERYTHING.
-    response.headers.set('Cache-Control', 'public, max-age=604800, immutable');
+    response.headers.set('Cache-Control', 'public, s-maxage=86400, max-age=86400, immutable');
+    response.headers.set('CDN-Cache-Control', 'public, s-maxage=86400, max-age=86400, immutable');
+    response.headers.set('Vercel-CDN-Cache-Control', 'public, s-maxage=604800, max-age=604800, immutable');
 
     return response;
 }
