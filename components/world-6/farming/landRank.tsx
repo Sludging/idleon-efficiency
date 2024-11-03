b1bc31edabd872ba12671e26b7e2b1c5518abece
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-11-03 11:54:07 +0100

    Updated Land Rank page
    Now use some TextAndLabel to display info and the page is just into a Grid instead of a Grid into a ComponentAndLabel

--- components/world-6/farming/landRank.tsx
+++ components/world-6/farming/landRank.tsx
@@ -20,47 +20,46 @@ export const LandRankDisplay = () => {
     return (
         <Box gap="medium" width="100%">
             <ShadowBox pad="small">
-            <ComponentAndLabel
-                label={`Total Land Ranks : ${farming.landRankPointsTotal} (points left to spend : ${farming.landRankPointsTotal-farming.landRankPointsSpent})`}
-                component={
-                    <Grid columns={{ size: 'auto', count: (size == "small" ? 2 : 5) }} fill>
-                        {
-                            farming.landrankDatabase.upgrades.map((upgrade, index) => {
-                                let label = "";
-                                if (!upgrade.unlocked) {
-                                    label = `Get ${upgrade.unlockThreshold - farming.landRankPointsTotal} more land ranks to unlock this`;
-                                }
+                <Box direction="row" gap="medium">
+                    <TextAndLabel label="Total Land Ranks :" text={farming.landRankPointsTotal.toString()} margin={{ bottom: 'small' }} />
+                    <TextAndLabel label="Points Left to Spend :" text={(farming.landRankPointsTotal-farming.landRankPointsSpent).toString()} margin={{ bottom: 'small' }} />
+                </Box>
+                <Grid columns={{ size: 'auto', count: (size == "small" ? 2 : 5) }} fill>
+                    {
+                        farming.landrankDatabase.upgrades.map((upgrade, index) => {
+                            let label = "";
+                            if (!upgrade.unlocked) {
+                                label = `Get ${upgrade.unlockThreshold - farming.landRankPointsTotal} more land ranks to unlock this`;
+                            }
 
-                                return (
-                                    <ShadowBox style={{ opacity: upgrade.level > 0 ? 1 : 0.6 }} key={index} background="dark-1" margin={{ right: 'small', bottom: 'small' }} pad="medium" gap="medium">
-                                        <Box gap="small" justify="between" fill>
-                                            <TextAndLabel textSize='xsmall' text={upgrade.getUpgradeBonusText()} label={`${upgrade.name} ${upgrade.level > 0 ? `(Lv. ${upgrade.level})` : ``}`} />
-                                            {
-                                                upgrade.unlocked ?
-                                                    <ComponentAndLabel
-                                                        label={label}
-                                                        component={
-                                                            <Box gap="xsmall" direction="row" align="center">
-                                                                <Text size="small">{upgrade.uniqueLevelBonus && upgrade.level > 0 ? "MAXED" : ""}</Text>
-                                                            </Box>                                                                       
-                                                        }
-                                                    />                                                        
-                                                :
+                            return (
+                                <ShadowBox style={{ opacity: upgrade.level > 0 ? 1 : 0.6 }} key={index} background="dark-1" margin={{ right: 'small', bottom: 'small' }} pad="medium" gap="medium">
+                                    <Box gap="small" justify="between" fill>
+                                        <TextAndLabel textSize='xsmall' text={upgrade.getUpgradeBonusText()} label={`${upgrade.name} ${upgrade.level > 0 ? `(Lv. ${upgrade.level})` : ``}`} />
+                                        {
+                                            upgrade.unlocked ?
                                                 <ComponentAndLabel
                                                     label={label}
                                                     component={
-                                                        <Box/>
+                                                        <Box gap="xsmall" direction="row" align="center">
+                                                            <Text size="small">{upgrade.uniqueLevelBonus && upgrade.level > 0 ? "MAXED" : ""}</Text>
+                                                        </Box>                                                                       
                                                     }
-                                                /> 
-                                            }                                            
-                                        </Box>
-                                    </ShadowBox>
-                                )
-                            })
-                        }
-                    </Grid>
-                }
-            />
+                                                />                                                        
+                                            :
+                                            <ComponentAndLabel
+                                                label={label}
+                                                component={
+                                                    <Box/>
+                                                }
+                                            /> 
+                                        }                                            
+                                    </Box>
+                                </ShadowBox>
+                            )
+                        })
+                    }
+                </Grid>
             </ShadowBox>
         </Box>
     )
