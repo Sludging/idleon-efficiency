9409bdc1c24f0b43228dfec648c00ae564e62a9b
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2024-11-11 00:20:01 +0100

    Updated The Tome page

--- app/account/tome/content.tsx
+++ app/account/tome/content.tsx
@@ -4,13 +4,8 @@ import {
     Box,
     Text,
     Heading,
-    Grid,
-    Stack
-} from 'grommet'
+    Grid} from 'grommet'
 import ShadowBox from '../../../components/base/ShadowBox';
-import IconImage from '../../../components/base/IconImage';
-import { Card } from '../../../data/domain/cards';
-import TipDisplay from '../../../components/base/TipDisplay';
 import { useAppDataStore } from '../../../lib/providers/appDataStoreProvider';
 import { useShallow } from 'zustand/react/shallow';
 import { TomeLine, Tome as TomeDomain } from '../../../data/domain/tome';
@@ -19,8 +14,8 @@ const LineDisplay = ({ line }: { line: TomeLine }) => {
     return (
         <ShadowBox background='dark-1' style={{ opacity: line.lineScore > 0 ? 1 : 0.5 }} gap='small' pad='medium' align='left'>
             <Box>
-                <Text>{line.title}</Text>
-                <Text>Score : {line.getLineScore()}</Text>
+                <Text>{line.currentValue} - {line.title} ({line.index})</Text>
+                <Text>Score : {line.getLineScore()}/{line.maxPoint}</Text>
                 {line.description != "" && <Text size='xsmall'>{line.description}</Text>}
             </Box>
         </ShadowBox>
