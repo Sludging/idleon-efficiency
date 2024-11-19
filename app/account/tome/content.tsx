17958e6dde4540f65d65ab0e3b935c3cdcac17ba
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2024-11-19 15:51:52 +0100

    Updated Tome page
    Now display a bar like in-game with similar colors
    
    Also display when account is in the "tome score leader board" like in-game

--- app/account/tome/content.tsx
+++ app/account/tome/content.tsx
@@ -4,21 +4,50 @@ import {
     Box,
     Text,
     Heading,
-    Grid} from 'grommet'
+    Grid,
+    Meter} from 'grommet'
 import ShadowBox from '../../../components/base/ShadowBox';
 import { useAppDataStore } from '../../../lib/providers/appDataStoreProvider';
 import { useShallow } from 'zustand/react/shallow';
-import { TomeLine, Tome as TomeDomain } from '../../../data/domain/tome';
-import { useMemo } from 'react';
+import { TomeLine, Tome as TomeDomain, TomeScoreColors } from '../../../data/domain/tome';
+import { CircleInformation } from 'grommet-icons';
+import TipDisplay from '../../../components/base/TipDisplay';
 
 const LineDisplay = ({ line }: { line: TomeLine }) => {
     const lineDescription = line.getLineDescription();
     return (
         <ShadowBox background='dark-1' style={{ opacity: line.lineScore > 0 ? 1 : 0.5 }} gap='small' pad='medium' align='left'>
-            <Box>
-                <Text>{line.getCurrentValueDisplay()} - {line.getLineName()} ({line.index})</Text>
-                <Text>Score : {line.lineScore}/{line.data.totalVal}</Text>
-                {lineDescription != "" && <Text size='xsmall'>{lineDescription}</Text>}
+            <Box direction='column' justify='center' gap="small">
+                <Box direction='row' justify='between'>
+                    <Box direction='row' gap="small" wrap>
+                        <Text>{line.getCurrentValueDisplay()} - {line.getLineName()}</Text>
+                        {lineDescription != "" && <TipDisplay
+                            heading={line.getLineName()}
+                            size='medium'
+                            maxWidth='medium'
+                            body={
+                                <Box>
+                                    <Text size='small'>{lineDescription}</Text>
+                                </Box>
+                            }
+                        >
+                            <CircleInformation size="small" />
+                        </TipDisplay>}
+                    </Box>
+                    <Text color={line.getLineDisplayColor()}>{line.lineScore}</Text>
+                </Box>
+                <Box>
+                    <Meter
+                        size="full"
+                        type="bar"
+                        alignSelf='center'
+                        thickness='xsmall'
+                        round
+                        background={TomeScoreColors.Background}
+                        color={line.getLineDisplayColor()}
+                        value = {line.lineScore}
+                        max={line.data.totalVal} />
+                </Box>
             </Box>
         </ShadowBox>
     )
@@ -34,11 +63,13 @@ function TomeDisplay() {
         return null;
     }
 
+    const rank = tome.getScoreRankingDisplay();
+
     return (
         <Box gap='medium'>
             <Heading level='2' size='medium' style={{ fontWeight: 'normal' }}>The Tome</Heading>
             <Box>
-                <Text>Total score : {tome.totalScore}</Text>
+                <Text>Total score : {tome.totalScore}{rank != '' ? ` (${rank})` : ''}</Text>
             </Box>
             <Grid columns={{ size: 'auto', count: 2 }} gap='small'>
                 {
