670b6028b48d6b74ef02c7f8f528d1a5ed99440c
Sludgadin <LLJKSom0@gmail.com>
2024-12-14 23:05:20 +1100

    bump versions across the board

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
@@ -18,13 +22,12 @@ if (process.env.VERCEL_ENV == "preview") {
 const moduleExports = {
   output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
   experimental: {
-    optimizePackageImports: ['styled-components', 'grommet', 'grommet-icons', 'firebase', 'firebaseui'],
+    optimizePackageImports: ['styled-components', 'grommet', 'grommet-icons', 'firebase'],
   },
   images: {
     dangerouslyAllowSVG: true,
@@ -42,10 +45,6 @@ const moduleExports = {
       },
     ],
   },
-  sentry: {
-    hideSourceMaps: true,
-    disableServerWebpackPlugin: true,
-  },
   async redirects() {
     return [
       {
@@ -62,18 +61,9 @@ const moduleExports = {
   //output: "export",
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports, {
+  silent: true,
+  disableServerWebpackPlugin: true,
+});
