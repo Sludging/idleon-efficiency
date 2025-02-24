e90cd6c4fcd648bebb6ac1473a580980f0309fcf
Sludgadin <LLJKSom0@gmail.com>
2025-02-24 22:57:57 +1100

    improve handling of very expensive values

--- components/coinsDisplay.tsx
+++ components/coinsDisplay.tsx
@@ -5,7 +5,7 @@ import {
 } from 'grommet'
 import { ImageData } from '../data/domain/imageData';
 
-import { Coins } from '../data/utility';
+import { Coins, nFormatter } from '../data/utility';
 import IconImage from './base/IconImage';
 
 interface CoinProps {
@@ -23,6 +23,19 @@ export default function CoinsDisplay(props: CoinProps) {
             height: 21, //coin.valueOf() > 5 ? 23 : 21,
             width: 21, //coin.valueOf() > 5 ? 27 : 21
         }
+    }
+
+    // Special handling for the most expensive coin, if quantity is greater than 10000
+    if (props.coinMap.has(Coins.Eclipse) && props.coinMap.get(Coins.Eclipse)! > 10000) {
+        const coin = Coins.Eclipse;
+        const value = props.coinMap.get(coin)!;
+        // We return a formatted value of only the most expensive coin.
+        return (
+            <Box key={`coin-${coin.valueOf()}`} direction="row" gap="xsmall">
+                <IconImage data={getCoinClass(coin)} scale={props.coinScale ? props.coinScale : 1} />
+                <Text>{nFormatter(value)}</Text>
+            </Box>
+        )
     }
 
     return (
