7c1604fd6c4b929ad9f20d19d2ef7ff4c1019c4e
Sludgadin <LLJKSom0@gmail.com>
2025-03-14 18:08:22 +1100

    update change log

--- data/changelog.ts
+++ data/changelog.ts
@@ -7,6 +7,14 @@ export interface ChangelogEntryData {
 
 export const changelogData: ChangelogEntryData[] = [
   {
+    version: "1.2.29",
+    title: "Steam Login",
+    date: "March 13, 2025",
+    changes: [
+      "Added Steam login button to the login page.",
+    ]
+  },
+  {
     version: "1.2.28",
     title: "Grimoire and Upgrade Vault",
     date: "February 27, 2025",
@@ -14,14 +22,6 @@ export const changelogData: ChangelogEntryData[] = [
       "Added grimoire page to track grimoire upgrades. Make sure to checkout the 'unlock path' feature.",
       "Added upgrade vault page to track upgrade vault",
       "Added change log page to track site updates",
-    ]
-  },
-  {
-    version: "1.2.29",
-    title: "Steam Login",
-    date: "March 13, 2025",
-    changes: [
-      "Added Steam login button to the login page.",
     ]
   },
 ];
