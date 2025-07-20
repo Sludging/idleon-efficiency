6b5cff946f2ff6dd798c8404ddf50dfb5c485372
Sludgadin <LLJKSom0@gmail.com>
2025-07-20 18:06:54 +1000

    CCPA handling

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import IconLink from "../base/IconLink"
 import Icon from "../leaderboards/icon"
 import Discord from "../../lib/discord"
+import "./footer.css"
 
 export const FooterComponent = () => {
     return (
@@ -20,25 +21,14 @@ export const FooterComponent = () => {
                     <Box align="center" pad="small">
                         <Text size="12px" color="grey-2">|</Text>
                     </Box>
-                    <Box direction="row" align="center" gap="small">
-                        <Box>
-                            <a href="https://www.playwire.com/contact-direct-sales" rel="noopener" target="_blank">
-                                <img src="https://www.playwire.com/hubfs/Powered-by-Playwire-Badges/Ads-Powered-by-playwire-2021-standalone-small-white-300px.png" alt="Ads-Powered-by-playwire-2021-standalone-small-white-300px" loading="lazy" style={{ width: "150px", height: '40px', display: "block" }}></img>
-                            </a>
-                        </Box>
-                        <Box>
-                            <IconLink href="https://www.playwire.com/contact-direct-sales" text="Advertise on this site." target="_blank" />
-                            {/* <a style={{ textDecoration: 'none', color: "#828D99", fontWeight: 'bold' }} color="#828D99" href="https://www.playwire.com/contact-direct-sales" rel="noopener" target="_blank">Advertise on this site.</a> */}
-                        </Box>
-                    </Box>
-                    <Box align="center" pad="small">
-                        <Text size="12px" color="grey-2">|</Text>
-                    </Box>
                     <IconLink icon={Icon} href="https://www.idleonefficiency.com/privacy-policy" text="Privacy Policy" target="_self" />
                     <Box align="center" pad="small">
                         <Text size="12px" color="grey-2">|</Text>
                     </Box>
                     <IconLink icon={Discord} href="https://discord.gg/AfsyBkSd2q" text="Idleon Efficiency" />
+                    <Box align="center" pad="small">
+                        <span className="ccpa-link" data-ccpa-link="1"></span>
+                    </Box>
                 </Box>
                 <Box justify="end" direction="row" gap="medium">
                     <Anchor href="https://www.buymeacoffee.com/sludger" target="_blank"><Image
