9fe5fae68d940bc5a410341648cd014eaaf0c4aa
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2026-01-04 11:05:22 +0100

    Added ingame indexes
    Just to be sure about it next time someone works on it

--- data/domain/enum/tomeScalingEnum.ts
+++ data/domain/enum/tomeScalingEnum.ts
@@ -1,7 +1,7 @@
 export enum TomeScalingEnum {
-    decay = "decay",
-    decayLog = "decayLog",
-    linearToMax = "linearToMax",
-    inverseDecay = "inverseDecay",
-    boundedDecay = "boundedDecay"
+    decay = "decay", // 0
+    decayLog = "decayLog", // 1
+    linearToMax = "linearToMax", // 2
+    inverseDecay = "inverseDecay", // 3
+    boundedDecay = "boundedDecay" // 4
 }
