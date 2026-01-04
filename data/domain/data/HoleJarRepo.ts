6de1d60e760ed27f101ac33dd1fb3c410324a017
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2026-01-04 18:13:15 +0100

    Small renames

--- data/domain/data/HoleJarRepo.ts
+++ data/domain/data/HoleJarRepo.ts
@@ -1,48 +1,48 @@
 import { HoleJarModel } from "../model/holeJarModel";
 
-export class HoleJarInfoBase { constructor(public index: number, public data: HoleJarModel) { } }
+export class HoleJarBase { constructor(public index: number, public data: HoleJarModel) { } }
 
 
 
 export const initHoleJarRepo = () => {
     return [
-        new HoleJarInfoBase(0, <HoleJarModel>{
+        new HoleJarBase(0, <HoleJarModel>{
             name: "Simple Jar",
             description: "Contains a random basic rupie"
         }),
-        new HoleJarInfoBase(1, <HoleJarModel>{
+        new HoleJarBase(1, <HoleJarModel>{
             name: "Tall Jar",
             description: "Has a {% chance to contain an Opal"
         }),
-        new HoleJarInfoBase(2, <HoleJarModel>{
+        new HoleJarBase(2, <HoleJarModel>{
             name: "Ornate Jar",
             description: "Has a {% chance to contain a new collectible"
         }),
-        new HoleJarInfoBase(3, <HoleJarModel>{
+        new HoleJarBase(3, <HoleJarModel>{
             name: "Great Jar",
             description: "Contains a random decent rupie"
         }),
-        new HoleJarInfoBase(4, <HoleJarModel>{
+        new HoleJarBase(4, <HoleJarModel>{
             name: "Enchanted Jar",
             description: "Has a {% chance to enchant a collectible doubling its bonus"
         }),
-        new HoleJarInfoBase(5, <HoleJarModel>{
+        new HoleJarBase(5, <HoleJarModel>{
             name: "Artisan Jar",
             description: "Contains a white rupee"
         }),
-        new HoleJarInfoBase(6, <HoleJarModel>{
+        new HoleJarBase(6, <HoleJarModel>{
             name: "Epic Jar",
             description: "Contains a random elegant rupie"
         }),
-        new HoleJarInfoBase(7, <HoleJarModel>{
+        new HoleJarBase(7, <HoleJarModel>{
             name: "Gilded Jar",
             description: "Doubles the amount of rupies from the next jar opened"
         }),
-        new HoleJarInfoBase(8, <HoleJarModel>{
+        new HoleJarBase(8, <HoleJarModel>{
             name: "Ceremony Jar",
             description: "Contains a dark rupie"
         }),
-        new HoleJarInfoBase(9, <HoleJarModel>{
+        new HoleJarBase(9, <HoleJarModel>{
             name: "Heirloom Jar",
             description: "Contains the one and only master rupie"
         }),
