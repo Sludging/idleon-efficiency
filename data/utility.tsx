741110d9da7984c6b88146a1c4f67cabbf14e3e6
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2026-01-17 12:29:32 +0100

    Updated utility to be able to use number2letterArray

--- data/utility.tsx
+++ data/utility.tsx
@@ -453,7 +453,7 @@ export const parseFormattedNumber = (formattedNumber: string | number): number =
   return parseFloat(number) * multiplier;
 };
 
-const number2letterArray = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+export const number2letterArray = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
     's',
     't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
