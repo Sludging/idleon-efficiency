6da4fcf47dabc78ac854625eb8832f707fbb99c7
Zoma-Ancestral <foltzer.matthieu@gmail.com>
2026-01-28 13:57:27 +0100

    Updated Model and Repo to also have name
    The name is also the name of the item used as currency to unlock it (the first one is using the Coral Reef corals tho), might be used in UI later

--- data/domain/data/DancingCoralBonusRepo.ts
+++ data/domain/data/DancingCoralBonusRepo.ts
@@ -7,46 +7,55 @@ export class DancingCoralBonusBase { constructor(public index: number, public da
 export const initDancingCoralBonusRepo = () => {
     return [
         new DancingCoralBonusBase(0, <DancingCoralBonusModel>{
+            name: "Reef Coral",
             desc: "+{% Daily Reef Coral",
             powerReq: 20,
             bonus: 2
         }),
         new DancingCoralBonusBase(1, <DancingCoralBonusModel>{
+            name: "Vibrant Coral",
             desc: "+{% Spelunking POW",
             powerReq: 100,
             bonus: 5
         }),
         new DancingCoralBonusBase(2, <DancingCoralBonusModel>{
+            name: "Glowing Coral",
             desc: "+{% Spelunking Amber gain",
             powerReq: 250,
             bonus: 3
         }),
         new DancingCoralBonusBase(3, <DancingCoralBonusModel>{
+            name: "Neon Coral",
             desc: "}x Class EXP gain",
             powerReq: 1000,
             bonus: 4
         }),
         new DancingCoralBonusBase(4, <DancingCoralBonusModel>{
+            name: "Twisted Coral",
             desc: "+{% who knows",
             powerReq: 3000,
             bonus: 1
         }),
         new DancingCoralBonusBase(5, <DancingCoralBonusModel>{
+            name: "6th Coral",
             desc: "+{% who knows",
             powerReq: 10000,
             bonus: 1
         }),
         new DancingCoralBonusBase(6, <DancingCoralBonusModel>{
+            name: "7th Coral",
             desc: "+{% who knows",
             powerReq: 25000,
             bonus: 1
         }),
         new DancingCoralBonusBase(7, <DancingCoralBonusModel>{
+            name: "8th Coral",
             desc: "+{% who knows",
             powerReq: 100000,
             bonus: 1
         }),
         new DancingCoralBonusBase(8, <DancingCoralBonusModel>{
+            name: "9th Coral",
             desc: "+{% who knows",
             powerReq: 500000,
             bonus: 1
--- data/domain/model/dancingCoralBonus.ts
+++ data/domain/model/dancingCoralBonus.ts
@@ -1,4 +1,5 @@
 export interface DancingCoralBonusModel {
+    name: string,
     desc: string,
     powerReq: number,
     bonus: number,
