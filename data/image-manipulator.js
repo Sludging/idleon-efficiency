25c62550f3703c70c7b99cfa02b025ed103e6f2e
Sludgadin <LLJKSom0@gmail.com>
2026-02-22 15:28:24 +1100

    fix image manipulator bug

--- data/image-manipulator.js
+++ data/image-manipulator.js
@@ -1,48 +1,48 @@
 (async () => {
     const fs = require('graceful-fs');
     const PNG = require('pngjs').PNG;
-    const path = require('path');
-    const pixelmatch = require('pixelmatch');
-    const getDirName = require('path').dirname;
+    const pixelmatch = (await import('pixelmatch')).default;
 
     const assetFolder = "../apk/assets/assets/data/";
     await fs.readdir(assetFolder, {}, async (err, files) => {
         files.forEach(async file => {
-                if (!file.includes(".png") || file.substring(0,4) == "Plat" || file.includes("bg.png") || file.includes("menuserver")) {
-                    return;
-                }
+            if (!file.includes(".png") || file.substring(0, 4) == "Plat" || file.includes("bg.png") || file.includes("menuserver")) {
+                return;
+            }
 
-                try {
+            try {
+                const img = PNG.sync.read(fs.readFileSync(`${assetFolder}/${file}`));
+                const img2 = PNG.sync.read(fs.readFileSync(`icons/assets/data/${file}`));
+                const { width, height } = img;
+
+                const res = pixelmatch(img.data, img2.data, null, width, height, { threshold: 0.1 })
+                if (res != 0) {
+                    console.log(`Images didn't match for: ${file}`);
+                    fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
+                }
+                /* } else {
+                    console.log(`Images matched for: ${file}`);
+                } */
+            }
+            catch (e) {
+                if (e.message && e.message.includes("Image sizes do not match")) {
+                    console.log(`Images size didn't match for: ${file}`);
+                    fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
+                } else if (e.message && e.message.includes("no such file or directory")) {
+                    console.log(`New Icon: ${file}`)
                     const img = PNG.sync.read(fs.readFileSync(`${assetFolder}/${file}`));
-                    const img2 = PNG.sync.read(fs.readFileSync(`icons/assets/data/${file}`));
-                    const { width, height } = img;
-
-                    const res = pixelmatch(img.data, img2.data, null, width, height, { threshold: 0.1 })
-                    if (res != 0) {
-                        console.log(`Images didn't match for: ${file}`);
-                        fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
-                    }
-                    /* } else {
-                        console.log(`Images matched for: ${file}`);
-                    } */
+                    var buffer = PNG.sync.write(img);
+                    await fs.writeFile(`icons/assets/data/${file}`, buffer, function (err) {
+                        if (err) {
+                            console.log(err);
+                            throw err;
+                        }
+                    })
                 }
-                catch (e) {
-                    if (e.message && e.message.includes("Image sizes do not match")) {
-                        console.log(`Images size didn't match for: ${file}`);
-                        fs.copyFileSync(`${assetFolder}/${file}`, `icons/assets/data/${file}`)
-                    }
-                    else {
-                        console.log(`New Icon: ${file}`)
-                        const img = PNG.sync.read(fs.readFileSync(`${assetFolder}/${file}`));
-                        var buffer = PNG.sync.write(img);
-                        await fs.writeFile(`icons/assets/data/${file}`, buffer, function (err) {
-                            if (err) {
-                                console.log(err);
-                                throw err;
-                            }
-                        })
-                    }
+                else {
+                    console.log("Failed with", e)
                 }
-            })
+            }
         })
+    })
 })();
